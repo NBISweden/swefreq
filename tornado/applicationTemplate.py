2ae6a580df88dea58583cb1d22a7eac07b8c9b89
Andreas Kusalananda Kähäri <andreas.kahari@bils.se>
2016-09-28 10:51:51 +0200

    Reindent.

--- tornado/applicationTemplate.py
+++ tornado/applicationTemplate.py
@@ -2,118 +2,128 @@ indexHead="""
 <!DOCTYPE html>
 <!-- define angular app -->
 <html ng-app="App">
-<head>
-  <!-- SCROLLS -->
-  <meta charset="utf-8">
-  <!-- Bootstrap -->
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.css" />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-  <!-- Angular -->
-  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular-route.min.js"></script>
-  <!-- Angular grid gui -->
-    <link rel="stylesheet" href="/javascript/main.css" type="text/css">
-    <link rel="stylesheet" href="/javascript/local.css" type="text/css">
-  <!-- The application -->
-  <script src="javascript/app.js"></script>
-</head>
+  <head>
+    <!-- SCROLLS -->
+    <meta charset="utf-8" />
+    <!-- Bootstrap -->
+    <link rel="stylesheet"
+          href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+    <link rel="stylesheet"
+          href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js">
+    </script>
+    <!-- Angular -->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js">
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular-route.min.js">
+    </script>
+    <!-- Angular grid gui -->
+    <link rel="stylesheet"
+          href="/javascript/main.css"
+          type="text/css" />
+    <link rel="stylesheet"
+          href="/javascript/local.css"
+          type="text/css" />
+    <!-- The application -->
+    <script src="javascript/app.js"></script>
+  </head>
 """
 indexHtml="""
-<!-- define angular controller -->
-<body ng-controller="mainController as mainCtrl">
-  <nav class="navbar navbar-default" height="60">
-    <div class="container">
-      <a class="navbar-brand pull-left" href="/#/"><img src="/javascript/swegen_logo_v1_trans_small.png"></a>
-      <ul class="nav navbar-nav navbar-right">
-           <a href="/#/dataBeacon/" role="button" class="btn btn-default">Data Beacon</a>
-           <a href="{{ExAC}}" role="button" class="btn btn-default">ExAC Browser</a>
-           <a href="/#/downloadData/" role="button" class="btn btn-default">Download Data</a>
-           &nbsp;
-           &nbsp;
-           <div class="btn-group navbar-btn">
-              {%if user_name != None%}
-                {{user_name}} <a href="/logout">Logout</a><br>{{email}}
-
-              {% else %}
-                <br>
-                <a href="/login">Login</a>
-              {% end %}
-           </div>
-      </ul>
+  <!-- define angular controller -->
+  <body ng-controller="mainController as mainCtrl">
+    <nav class="navbar navbar-default"
+         height="60">
+      <div class="container">
+        <a class="navbar-brand pull-left"
+           href="/#/">
+          <img src="/javascript/swegen_logo_v1_trans_small.png" />
+        </a>
+        <ul class="nav navbar-nav navbar-right">
+        <a href="/#/dataBeacon/"
+           role="button"
+           class="btn btn-default">Data Beacon</a>
+        <a href="{{ExAC}}"
+           role="button"
+           class="btn btn-default">ExAC Browser</a>
+        <a href="/#/downloadData/"
+           role="button"
+           class="btn btn-default">Download Data</a>&#160; &#160; 
+        <div class="btn-group navbar-btn">{%if user_name != None%}
+        {{user_name}} 
+        <a href="/logout">Logout</a>
+        <br />{{email}} {% else %} 
+        <br />
+        <a href="/login">Login</a>{% end %}</div></ul>
+      </div>
+    </nav>
+    <div id="main">
+      <!-- angular templating -->
+      <!-- this is where content will be injected by angular -->
+      <div ng-view=""></div>
     </div>
-  </nav>
-  <div id="main">
-    <!-- angular templating -->
-		<!-- this is where content will be injected by angular-->
-    <div ng-view></div>
-  </div>
-
-  <footer class="text-center">
-  </footer>
-
-</body>
+    <footer class="text-center"></footer>
+  </body>
 </html>
 """
 notAuthorizedHtml="""
-<!-- define angular controller -->
-<body ng-controller="mainController as mainCtrl">
-  <nav class="navbar navbar-default">
-    <div class="container">
-      <a class="navbar-brand pull-left" href="/#/"><img src="/javascript/swegen_logo_v1_trans_small.png"></a>
-      <ul class="nav navbar-nav navbar-right">
-           <a href="/#/dataBeacon/" role="button" class="btn btn-default">Data Beacon</a>
-           <a href="{{ExAC}}" role="button" class="btn btn-default">ExAC Browser</a>
-           &nbsp;
-           &nbsp;
-           <div class="btn-group navbar-btn">
-                <br>
-                <a href="/login">Login</a>
-           </div>
-      </ul>
+  <!-- define angular controller -->
+  <body ng-controller="mainController as mainCtrl">
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <a class="navbar-brand pull-left"
+           href="/#/">
+          <img src="/javascript/swegen_logo_v1_trans_small.png" />
+        </a>
+        <ul class="nav navbar-nav navbar-right">
+        <a href="/#/dataBeacon/"
+           role="button"
+           class="btn btn-default">Data Beacon</a>
+        <a href="{{ExAC}}"
+           role="button"
+           class="btn btn-default">ExAC Browser</a>&#160; &#160; 
+        <div class="btn-group navbar-btn">
+          <br />
+          <a href="/login">Login</a>
+        </div></ul>
+      </div>
+    </nav>
+    <div id="main">
+      <!-- angular templating -->
+      <!-- this is where content will be injected -->
+      <div ng-view=""></div>
     </div>
-  </nav>
-  <div id="main">
-    <!-- angular templating -->
-		<!-- this is where content will be injected -->
-    <div ng-view></div>
-  </div>
-</body>
+  </body>
 </html>
 """
 indexNoAccess="""
-<!-- define angular controller -->
-<body ng-controller="mainController as mainCtrl">
-  <nav class="navbar navbar-default">
-    <div class="container">
-      <a class="navbar-brand pull-left" href="/#/"><img src="/javascript/swegen_logo_v1_trans_small.png"></a>
-      <ul class="nav navbar-nav navbar-right">
-           <a href="/#/requestAccess/" role="button" class="btn btn-default">Request Access</a>
-           &nbsp;
-           &nbsp;
-           <div class="btn-group navbar-btn">
-              {%if user_name != None%}
-                {{user_name}} <a href="/logout">Logout</a><br>{{email}}
-
-              {% else %}
-                <br>
-                <a href="/login">Login</a>
-              {% end %}
-           </div>
-      </ul>
+  <!-- define angular controller -->
+  <body ng-controller="mainController as mainCtrl">
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <a class="navbar-brand pull-left"
+           href="/#/">
+          <img src="/javascript/swegen_logo_v1_trans_small.png" />
+        </a>
+        <ul class="nav navbar-nav navbar-right">
+        <a href="/#/requestAccess/"
+           role="button"
+           class="btn btn-default">Request Access</a>&#160; &#160; 
+        <div class="btn-group navbar-btn">{%if user_name != None%}
+        {{user_name}} 
+        <a href="/logout">Logout</a>
+        <br />{{email}} {% else %} 
+        <br />
+        <a href="/login">Login</a>{% end %}</div></ul>
+      </div>
+    </nav>
+    <div id="main">
+      <!-- angular templating -->
+      <!-- this is where content will be injected by angular -->
+      <div ng-view=""></div>
     </div>
-  </nav>
-
-  <div id="main">
-    <!-- angular templating -->
-		<!-- this is where content will be injected by angular-->
-    <div ng-view></div>
-  </div>
-
-  <footer class="text-center">
-  </footer>
-
-</body>
+    <footer class="text-center"></footer>
+  </body>
 </html>
 """
