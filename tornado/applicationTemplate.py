b12fda420bf53277123ced52ea05d759a14fa95d
Johan Viklund <johan.viklund@bils.se>
2016-10-19 13:16:08 +0200

    Updated google analytics tracking

--- tornado/applicationTemplate.py
+++ tornado/applicationTemplate.py
@@ -36,7 +36,7 @@ indexHead="""
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-44754195-1', 'auto');
+    ga('create', 'UA-85976442-1', 'auto');
     ga('send', 'pageview');
     </script>
     <!-- End Google Analytics -->
