f5ffe401849c77bd460acdd2b01880ab8cf4e576
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-04-19 11:07:48 +0200

    Nicify
    * up-case types
    * move PRIMARY KEY specifier to column declaration
    * remove unnecessary backticks
    * remove database name from table creation statements
    * remove field with specification from fields that don't need it
    * rename user_UNIQUE --> user_idx
    * don't quote integers, and don't use integers for booleans

--- sql/swefreq.sql
+++ sql/swefreq.sql
@@ -2,32 +2,30 @@
 -- as well as the tables. To run this file use:
 -- mysql -u root -p < swefreq.sql
 
-CREATE DATABASE IF NOT EXISTS `swefreq` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE IF NOT EXISTS swefreq /*!40100 DEFAULT CHARACTER SET latin1 */;
 CREATE USER IF NOT EXISTS swefreq@localhost IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON swefreq.* TO swefreq@localhost;
 FLUSH PRIVILEGES;
 USE swefreq;
 
-CREATE TABLE IF NOT EXISTS swefreq.users (
-  `pk` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) DEFAULT NULL,
-  `email` varchar(100) NOT NULL,
-  `download_count` int(11) DEFAULT '0',
-  `swefreq_admin` boolean DEFAULT '0',
-  `affiliation` varchar(100) DEFAULT NULL,
-  `full_user` boolean DEFAULT '0',
-  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `country` varchar(100) DEFAULT NULL,
-  `newsletter` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`pk`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
+CREATE TABLE IF NOT EXISTS users (
+  pk                INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username          VARCHAR(100) DEFAULT NULL,
+  email             VARCHAR(100) NOT NULL,
+  download_count    INTEGER DEFAULT 0,
+  swefreq_admin     BOOLEAN DEFAULT false,
+  affiliation       VARCHAR(100) DEFAULT NULL,
+  full_user         BOOLEAN DEFAULT false,
+  create_date       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  country           VARCHAR(100) DEFAULT NULL,
+  newsletter        TINYINT DEFAULT 0,
+  UNIQUE KEY email_idx (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-CREATE TABLE IF NOT EXISTS swefreq.user_log (
-  `pk` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) DEFAULT NULL,
-  `action` varchar(45) DEFAULT NULL,
-  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`pk`)
+CREATE TABLE IF NOT EXISTS user_log (
+  pk                INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email             VARCHAR(100) DEFAULT NULL,
+  action            VARCHAR(45) DEFAULT NULL,
+  ts                TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
