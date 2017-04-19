3657ed3a83409ec1b5c7a5b494e88674d1dfd0fe
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-04-19 11:16:46 +0200

    Separate type from column options, rename users table, pks

--- sql/swefreq.sql
+++ sql/swefreq.sql
@@ -8,24 +8,24 @@ GRANT ALL PRIVILEGES ON swefreq.* TO swefreq@localhost;
 FLUSH PRIVILEGES;
 USE swefreq;
 
-CREATE TABLE IF NOT EXISTS users (
-  pk                INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  username          VARCHAR(100) DEFAULT NULL,
-  email             VARCHAR(100) NOT NULL,
-  download_count    INTEGER DEFAULT 0,
-  swefreq_admin     BOOLEAN DEFAULT false,
-  affiliation       VARCHAR(100) DEFAULT NULL,
-  full_user         BOOLEAN DEFAULT false,
-  create_date       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  country           VARCHAR(100) DEFAULT NULL,
-  newsletter        TINYINT DEFAULT 0,
+CREATE TABLE IF NOT EXISTS user (
+  user_pk           INTEGER         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username          VARCHAR(100)    DEFAULT NULL,
+  email             VARCHAR(100)    NOT NULL,
+  download_count    INTEGER         DEFAULT 0,
+  swefreq_admin     BOOLEAN         DEFAULT false,
+  affiliation       VARCHAR(100)    DEFAULT NULL,
+  full_user         BOOLEAN         DEFAULT false,
+  create_date       TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  country           VARCHAR(100)    DEFAULT NULL,
+  newsletter        TINYINT         DEFAULT 0,
   UNIQUE KEY email_idx (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE IF NOT EXISTS user_log (
-  pk                INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  email             VARCHAR(100) DEFAULT NULL,
-  action            VARCHAR(45) DEFAULT NULL,
-  ts                TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  user_log_pk       INTEGER         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email             VARCHAR(100)    DEFAULT NULL,
+  action            VARCHAR(45)     DEFAULT NULL,
+  ts                TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
