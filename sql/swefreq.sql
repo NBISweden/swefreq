61e3ddf1b9e07c26888433094633861f113c58e3
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-04-19 11:21:55 +0200

    Add database_access table, move columns from user here

--- sql/swefreq.sql
+++ sql/swefreq.sql
@@ -13,12 +13,10 @@ CREATE TABLE IF NOT EXISTS user (
   username          VARCHAR(100)    DEFAULT NULL,
   email             VARCHAR(100)    NOT NULL,
   download_count    INTEGER         DEFAULT 0,
-  swefreq_admin     BOOLEAN         DEFAULT false,
   affiliation       VARCHAR(100)    DEFAULT NULL,
   full_user         BOOLEAN         DEFAULT false,
   create_date       TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
   country           VARCHAR(100)    DEFAULT NULL,
-  newsletter        TINYINT         DEFAULT 0,
   UNIQUE KEY email_idx (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -34,3 +32,12 @@ CREATE TABLE IF NOT EXISTS user_log (
   action            VARCHAR(45)     DEFAULT NULL,
   ts                TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS dataset_access (
+    dataset_access_pk   INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_pk             INTEGER     NOT NULL,
+    newsletter          BOOLEAN     DEFAULT false,
+    admin               BOOLEAN     DEFAULT false,
+    consented           BOOLEAN     DEFAULT false
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
