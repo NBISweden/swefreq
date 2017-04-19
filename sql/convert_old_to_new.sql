3f8c6335e4ceeb18529cf55dd745bf92c85c4d4d
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-04-19 12:00:53 +0200

    Comments

--- sql/convert_old_to_new.sql
+++ sql/convert_old_to_new.sql
@@ -2,6 +2,7 @@ USE swefreq;
 
 RENAME TABLE users TO users_old;
 RENAME TABLE user_log TO user_log_old;
+-- leaves tables users_old and user_log_old as is when done
 
 source swefreq.sql
 
@@ -12,9 +13,10 @@ INSERT INTO user
            create_date, full_user, download_count
     FROM users_old;
 
+-- since table dataset is empty, insert 1 for dataset_pk (will point to
+-- SweGen dataset later).
 INSERT INTO user_log
     (user_pk, dataset_pk, action, ts)
     SELECT user_pk, 1, action, ts
     FROM user JOIN user_log_old ON (user.email = user_log_old.email);
 
--- leaves tables users_old and user_log_old as is
