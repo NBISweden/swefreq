3f1374f0219ad773ec218ef601fc6c753c6b5541
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-04-19 13:34:19 +0200

    Various
    * Change email_idx index to be "CONSTRAINT UNIQUE"
    * Add missing comma
    * Add unique constraint on (dataset_pk, user_pk) in table dataset_access

--- sql/swefreq.sql
+++ sql/swefreq.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS user (
     country             VARCHAR(100)    DEFAULT NULL,
     create_date         TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     download_count      INTEGER         DEFAULT 0,
-    UNIQUE KEY email_idx (email)
+    CONSTRAINT UNIQUE (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS dataset (
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS user_log (
     dataset_pk          INTEGER         NOT NULL,
     action              VARCHAR(45)     DEFAULT NULL,
     ts                  TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT FOREIGN KEY (user_pk)    REFERENCES user(user_pk)
+    CONSTRAINT FOREIGN KEY (user_pk)    REFERENCES user(user_pk),
     CONSTRAINT FOREIGN KEY (dataset_pk) REFERENCES dataset(dataset_pk)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -42,7 +42,8 @@ CREATE TABLE IF NOT EXISTS dataset_access (
     wants_newsletter    BOOLEAN         DEFAULT false,
     is_admin            BOOLEAN         DEFAULT false,
     has_consented       BOOLEAN         DEFAULT false,
-    CONSTRAINT FOREIGN KEY (dataset_pk) REFERENCES dataset(dataset_pk)
+    CONSTRAINT UNIQUE (dataset_pk, user_pk),
+    CONSTRAINT FOREIGN KEY (dataset_pk) REFERENCES dataset(dataset_pk),
     CONSTRAINT FOREIGN KEY (user_pk)    REFERENCES user(user_pk)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
