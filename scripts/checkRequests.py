9cc7e7033b1e56701bbb86a5da524fc075bb3cce
Johan Viklund <johan.viklund@nbis.se>
2017-05-05 13:46:55 +0200

    Fixed spelling and added descriptive comment.

--- scripts/checkRequests.py
+++ scripts/checkRequests.py
@@ -28,14 +28,15 @@ try:
     msg             = email.mime.multipart.MIMEMultipart()
     msg['to']       = secrets.admin_address
     msg['from']     = secrets.from_address
-    msg['subject']  = 'Pending Swefreq requests'
+    msg['subject']  = 'Pending SweFreq requests'
     msg['reply-to'] = secrets.reply_to_address
-    body            = "There are pending requests for swefreq accounts, please visit http://swefreq.nbis.se"
+    body            = "There are pending requests for SweFreq accounts, please visit http://swefreq.nbis.se"
     msg.attach(MIMEText(body, 'plain'))
 
     server = smtplib.SMTP(secrets.mail_server)
     server.sendmail(msg['from'], [msg['to']], msg.as_string())
 except peewee.DoesNotExist:
+    # No new users so we don't send any emails.
     pass
 except Exception as e:
     print "Can't send email"
