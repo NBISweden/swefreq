3294861ec688a419d2db4ea5738948201ac21534
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-06-20 15:11:55 +0200

    Add dataset_logo table

--- sql/swefreq.sql
+++ sql/swefreq.sql
@@ -65,3 +65,11 @@ CREATE TABLE IF NOT EXISTS dataset_file (
     CONSTRAINT FOREIGN KEY (dataset_version_pk)
         REFERENCES dataset_version(dataset_version_pk)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS dataset_logo (
+    dataset_logo_pk     INTEGER         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    dataset_pk          INTEGER         NOT NULL,
+    mimetype            VARCHAR(50),
+    data                BLOB,
+    CONSTRAINT FOREIGN KEY (dataset_pk) REFERENCES dataset(dataset_pk)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
