938c07fc9dc4a50894115615684e7535dff5bacc
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-06-29 10:35:11 +0200

    Correct last commit (added to wrong table)

--- sql/swefreq.sql
+++ sql/swefreq.sql
@@ -14,11 +14,14 @@ CREATE TABLE IF NOT EXISTS user (
 
 CREATE TABLE IF NOT EXISTS dataset (
     dataset_pk          INTEGER         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    sample_set_pk       INTEGER         NOT NULL,
     short_name          VARCHAR(50)     NOT NULL,
     full_name           VARCHAR(100)    NOT NULL,
     browser_uri         VARCHAR(200)    DEFAULT NULL,
     beacon_uri          VARCHAR(200)    DEFAULT NULL,
-    CONSTRAINT UNIQUE (short_name)
+    CONSTRAINT UNIQUE (short_name),
+    CONSTRAINT FOREIGN KEY (sample_set_pk)
+        REFERENCES sample_set(sample_set_pk)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS user_log (
@@ -37,16 +40,13 @@ CREATE TABLE IF NOT EXISTS dataset_access (
     dataset_access_pk   INTEGER         NOT NULL PRIMARY KEY AUTO_INCREMENT,
     dataset_pk          INTEGER         NOT NULL,
     user_pk             INTEGER         NOT NULL,
-    sample_set_pk       INTEGER         NOT NULL,
     wants_newsletter    BOOLEAN         DEFAULT false,
     is_admin            BOOLEAN         DEFAULT false,
     has_consented       BOOLEAN         DEFAULT false,
     has_access          BOOLEAN         DEFAULT false,
     CONSTRAINT UNIQUE (dataset_pk, user_pk),
     CONSTRAINT FOREIGN KEY (dataset_pk) REFERENCES dataset(dataset_pk),
-    CONSTRAINT FOREIGN KEY (user_pk)    REFERENCES user(user_pk),
-    CONSTRAINT FOREIGN KEY (sample_set_pk)
-        REFERENCES sample_set(sample_set_pk)
+    CONSTRAINT FOREIGN KEY (user_pk)    REFERENCES user(user_pk)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS dataset_version (
