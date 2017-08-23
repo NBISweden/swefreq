de9b134f36dd5c5662811092b22ed762db85a65f
Johan Viklund <johan.viklund@nbis.se>
2017-08-23 10:42:04 +0000

    Stubbed out some argument handling

--- scripts/manage_datasets.py
+++ scripts/manage_datasets.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
+import argparse
+import re
+import sys
+
 import db
 
-print("Updating dataset_version 2")
-dv = db.DatasetVersion.select().where( db.DatasetVersion.dataset_version == 2 ).get()
+def update(dataset):
+    print("Updating dataset_version 2")
+    dv = db.DatasetVersion.select().where( db.DatasetVersion.dataset_version == 2 ).get()
 
-newterms = ""
-with open('/tmp/new-terms.txt') as f:
-    newterms = "".join(f.readlines())
+    newterms = ""
+    with open('/tmp/new-terms.txt') as f:
+        newterms = "".join(f.readlines())
 
-newdesc = ""
-with open('/tmp/new-description.txt') as f
-    newdesc = "".join(f.readlines())
+    newdesc = ""
+    with open('/tmp/new-description.txt') as f
+        newdesc = "".join(f.readlines())
 
-dv.terms = newterms
-dv.description = newdesc
-dv.save()
+    dv.terms = newterms
+    dv.description = newdesc
+    dv.save()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Manage datasets in database')
+    parser.add_argument('--dataset', help='Dataset id', required=True)
+    args = parser.parse_args()
+    update(args.dataset, args.file)
