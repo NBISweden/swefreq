8cfda624332b9bafa0894ec1bb0b64f8b47f2fe0
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-08-30 14:01:09 +0200

    Remove dataset_version.ts

--- sql/swefreq.sql
+++ sql/swefreq.sql
@@ -58,7 +58,6 @@ CREATE TABLE IF NOT EXISTS dataset_version (
     dataset_version_pk  INTEGER         NOT NULL PRIMARY KEY AUTO_INCREMENT,
     dataset_pk          INTEGER         NOT NULL,
     version             VARCHAR(20)     NOT NULL,
-    ts                  TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
     is_current          BOOLEAN         DEFAULT true,
     description         TEXT            NOT NULL,
     terms               TEXT            NOT NULL,
