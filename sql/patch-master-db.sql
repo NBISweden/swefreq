51d343a3a694d1da71b884f52a34ae1afcdbea39
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-09-05 12:45:56 +0200

    Add view to patch

--- sql/patch-master-db.sql
+++ sql/patch-master-db.sql
@@ -101,3 +101,11 @@ ALTER TABLE dataset_version ADD COLUMN (
 );
 
 ALTER TABLE dataset_version DROP COLUMN ts;
+
+-- add the view
+
+CREATE OR REPLACE VIEW dataset_version_current AS
+    SELECT * FROM dataset_version
+    WHERE (dataset_pk,version) IN (
+        SELECT dataset_pk, MAX(version) FROM dataset_version
+        GROUP BY dataset_pk );
