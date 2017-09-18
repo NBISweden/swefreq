286c259e3482687ac478370ec5a8eb5897349ab8
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-09-18 13:49:08 +0200

    Use `timeout` from GNU coreutils

--- test/01_daemon_starts.sh
+++ test/01_daemon_starts.sh
@@ -3,17 +3,8 @@
 # This script just tries to make sure that the tornado webserver starts and
 # doesn't crash immediately.
 
-python route.py &
-pid=$!
-
-# sleep for 5 seconds
-for i in {1..5}; do
-    sleep 1
-    if ! kill -0 $pid; then
-        exit 1
-    fi
-done
-
-kill $pid
-wait $pid
-exit 0
+if timeout 5 python route.py; then
+    exit 0
+else
+    exit 1
+fi
