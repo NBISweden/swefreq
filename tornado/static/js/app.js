938ed3cecba4a72c352c3caa239f0f476f83c364
Johan Viklund <johan.viklund@nbis.se>
2017-09-28 11:29:05 +0000

    Introduced whitespace for nicer alignment in next commit

--- tornado/static/js/app.js
+++ tornado/static/js/app.js
@@ -422,14 +422,14 @@
     // configure routes
     App.config(function($routeProvider, $locationProvider) {
         $routeProvider
-            .when('/',                          { templateUrl: 'static/js/ng-templates/home.html'             })
-            .when('/dataBeacon/',               { templateUrl: 'static/js/ng-templates/dataBeacon.html'       })
-            .when('/dataset/:dataset',          { templateUrl: 'static/js/ng-templates/dataset.html'          })
-            .when('/dataset/:dataset/terms',    { templateUrl: 'static/js/ng-templates/dataset-terms.html'    })
-            .when('/dataset/:dataset/download', { templateUrl: 'static/js/ng-templates/dataset-download.html' })
-            .when('/dataset/:dataset/beacon',   { templateUrl: 'static/js/ng-templates/dataset-beacon.html'   })
-            .when('/dataset/:dataset/admin',    { templateUrl: 'static/js/ng-templates/dataset-admin.html'    })
-            .otherwise(                         { templateUrl: 'static/js/ng-templates/404.html'              });
+            .when('/',                                  { templateUrl: 'static/js/ng-templates/home.html'             })
+            .when('/dataBeacon/',                       { templateUrl: 'static/js/ng-templates/dataBeacon.html'       })
+            .when('/dataset/:dataset',                  { templateUrl: 'static/js/ng-templates/dataset.html'          })
+            .when('/dataset/:dataset/terms',            { templateUrl: 'static/js/ng-templates/dataset-terms.html'    })
+            .when('/dataset/:dataset/download',         { templateUrl: 'static/js/ng-templates/dataset-download.html' })
+            .when('/dataset/:dataset/beacon',           { templateUrl: 'static/js/ng-templates/dataset-beacon.html'   })
+            .when('/dataset/:dataset/admin',            { templateUrl: 'static/js/ng-templates/dataset-admin.html'    })
+            .otherwise(                                 { templateUrl: 'static/js/ng-templates/404.html'              });
 
         // Use the HTML5 History API
         $locationProvider.html5Mode(true);
