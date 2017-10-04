8aa63abd1eb9440eaa4aa330348366ee74bfc93d
Johan Viklund <johan.viklund@nbis.se>
2017-10-04 14:11:00 +0000

    DatasetVersions drop-down in UI

--- tornado/static/js/app.js
+++ tornado/static/js/app.js
@@ -234,7 +234,7 @@
 
     /////////////////////////////////////////////////////////////////////////////////////
 
-    App.controller('navbarController', ['$routeParams', 'Dataset', function($routeParams, Dataset) {
+    App.controller('navbarController', ['$routeParams', 'Dataset', 'DatasetVersions', function($routeParams, Dataset, DatasetVersions) {
         var localThis = this;
         localThis.is_admin = false;
 
@@ -242,10 +242,20 @@
                 localThis.is_admin    = data.dataset.is_admin;
                 localThis.dataset     = data.dataset.short_name;
                 localThis.browser_uri = data.dataset.browser_uri;
-                localThis.urlBase = '/dataset/' + localThis.dataset;
+                localThis.urlBase     = '/dataset/' + localThis.dataset;
+                localThis.thisVersion = data.dataset.version.version;
                 if ($routeParams['version']) {
                     localThis.urlBase += '/version/' + $routeParams['version'];
                 }
+                DatasetVersions(localThis.dataset).then(function(data) {
+                    for (var ii = 0; ii < data.length; ii++) {
+                        if ( data[ii].name == localThis.thisVersion ) {
+                            data[ii].current = true;
+                            break;
+                        }
+                    }
+                    localThis.versions = data;
+                });
             }
         );
 
