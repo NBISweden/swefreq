3a9a35654573909c3e626477be768d8d443a3fe6
Johan Viklund <johan.viklund@nbis.se>
2017-10-05 11:41:47 +0000

    DatasetVersions factory

--- tornado/static/js/app.js
+++ tornado/static/js/app.js
@@ -10,6 +10,18 @@
         };
     });
 
+    App.factory('DatasetVersions', function($http, $q) {
+        return function(dataset) {
+            // Hide the ugly implementation details by using $q to return an
+            // async object that resolves to the content of the REST call.
+            return $q(function(resolve,reject) {
+                $http.get('/api/datasets/' + dataset + '/versions')
+                    .then(function(data) {
+                        resolve(data.data.data);
+                    })
+            });
+        };
+    });
 
     App.factory('DatasetUsers', function($http, $cookies, $q) {
         var service = {};
