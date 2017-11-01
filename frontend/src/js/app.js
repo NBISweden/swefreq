b631fa42f8cff7ea0d3fc75616f29dc6d89323a0
Johan Viklund <johan.viklund@nbis.se>
2017-11-01 13:36:25 +0000

    Moved app.js

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -226,10 +226,17 @@
 
     /////////////////////////////////////////////////////////////////////////////////////
 
-    App.controller('mainController', function($location) {
+    App.controller('mainController', [ '$location', 'User', function($location, User) {
         var localThis = this;
         localThis.url = function () { return $location.path() };
-    });
+        localThis.logged_in = false;
+        User().then(function(data) {
+            localThis.user = data.data;
+            if ( localThis.user.user !== null ) {
+                localThis.logged_in = true;
+            }
+        });
+    }]);
 
     /////////////////////////////////////////////////////////////////////////////////////
 
