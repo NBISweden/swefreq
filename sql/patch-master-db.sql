7a3ce55026acc7dfb55536f9e8148dcab4bb8c10
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-11-13 13:49:33 +0100

    Do it right

--- sql/patch-master-db.sql
+++ sql/patch-master-db.sql
@@ -5,7 +5,10 @@
 -- Add mongodb_collection to the dataset table
 
 ALTER TABLE dataset ADD COLUMN
-    mongodb_collection  VARCHAR(50)     NOT NULL;
+    mongodb_collection VARCHAR(50);
 
 UPDATE dataset SET mongodb_collection = 'exac'
     WHERE dataset_pk = 1;
+
+ALTER TABLE dataset MODIFY COLUMN mongodb_collection
+    mongodb_collection VARCHAR(50) NOT NULL;
