95b399f5aaa76fde4836326614c6ed5fd106a8f4
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-11-13 13:39:50 +0100

    Patch updated

--- sql/patch-master-db.sql
+++ sql/patch-master-db.sql
@@ -132,3 +132,11 @@ ALTER TABLE user_access_log
     CHANGE action
         action  ENUM ('access_requested','access_granted','access_revoked',
                       'private_link');
+
+-- Add mongodb_collection to the dataset table
+
+ALTER TABLE dataset ADD COLUMN
+    mongodb_collection  VARCHAR(50)     NOT NULL;
+
+UPDATE dataset SET mongodb_collection = 'exac'
+    WHERE dataset_pk = 1;
