e85e21f430eb9d50f448bae49187debb6931836d
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-11-21 15:28:35 +0100

    Connect to database and insert study

--- scripts/metadata-loader.pl
+++ scripts/metadata-loader.pl
@@ -20,8 +20,26 @@ my $json_text;
 print "$json_text";
 my $data = decode_json($json_text);
 
-print Dumper($data);
-exit;
+if ( -f $data->{'study'}{'description'} ) {
+    local $/;
+    my $fh = IO::File->new( $data->{'study'}{'description'}, "r" ) or die($!);
+    $data->{'study'}{'description'} = <$fh>;
+    $fh->close();
+}
 
-my $dsn = 'DBI:mysql:database=swefreq;host=swefreq-db-dev';
-my $dbh = DBI->connect( $dsn, 'swefreq' );
+my $dbh = DBI->connect( 'DBI:mysql:database=swefreq;host=swefreq-db-dev',
+    'swefreq', undef, { 'RaisError' => 1 } );
+
+$dbh->do(
+    'INSERT INTO study '
+      . '(pi_name,pi_email,contact_name,contact_email,'
+      . 'title,description,publication_date,ref_doi) '
+      . 'VALUES (?,?,?,?,?,?,?,?)',
+    undef,
+    @{ $data->{'study'} }{
+        'pi-name',          'pi-email',
+        'contact-name',     'contact-email',
+        'title',            'description',
+        'publication-date', 'ref-doi'
+    }
+);
