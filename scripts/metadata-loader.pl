fd6ecbf1e7ce0ed7853aaeb6a0af00e4ada0feb9
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-11-22 11:07:59 +0100

    Move database connection around a bit, comments

--- scripts/metadata-loader.pl
+++ scripts/metadata-loader.pl
@@ -22,14 +22,15 @@ sub get_file {
 my $json_text = get_file('metadata-example.json');
 my $data = decode_json($json_text);
 
+my $dbh = DBI->connect( 'DBI:mysql:database=swefreq;host=swefreq-db-dev',
+    'swefreq', undef, { 'RaiseError' => 1 } );
+
 if ( -f $data->{'study'}{'description'} ) {
     $data->{'study'}{'description'} =
       get_file( $data->{'study'}{'description'} );
 }
 
-my $dbh = DBI->connect( 'DBI:mysql:database=swefreq;host=swefreq-db-dev',
-    'swefreq', undef, { 'RaiseError' => 1 } );
-
+# Insert study
 $dbh->do(
     'INSERT IGNORE INTO study '
       . '(pi_name,pi_email,contact_name,contact_email,'
@@ -45,6 +46,7 @@ $dbh->do(
 );
 
 foreach my $dataset ( @{ $data->{'study'}{'datasets'} } ) {
+    # Insert dataset
     $dbh->do(
         'INSERT IGNORE INTO dataset '
           . '(study_pk,short_name,full_name,avg_seq_depth,'
