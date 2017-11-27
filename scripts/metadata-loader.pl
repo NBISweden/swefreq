d62bd9cb603aa96f8150541b0553fbe1e76e1aee
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-11-27 17:23:20 +0100

    perltidy (to Ensembl stylesheet, but with 4 space indent rather than 2)

--- scripts/metadata-loader.pl
+++ scripts/metadata-loader.pl
@@ -39,7 +39,8 @@ sub validate_required {
     foreach my $key (@keys) {
         if ( !exists( $variable->{$key} ) ) {
             ++$error;
-            printf( STDERR "%s is missing required key %s\n", $name, $key );
+            printf( STDERR "%s is missing required key %s\n",
+                    $name, $key );
         }
     }
 
@@ -66,20 +67,20 @@ my $dbh = DBI->connect( sprintf( "DBI:mysql:database=%s;host=%s",
                         { 'RaiseError' => 1 } );
 
 die
-    if validate_required(
+  if validate_required(
     $study,
     'study',
     qw( title publication-date pi-name pi-email contact-name contact-email datasets )
-    );
+  );
 
 # Insert study
 if ( exists( $study->{'description'} ) && -f $study->{'description'} ) {
     $study->{'description'} = get_file( $study->{'description'} );
 }
-$dbh->do( 'INSERT IGNORE INTO study '
-              . '(pi_name,pi_email,contact_name,contact_email,'
-              . 'title,description,publication_date,ref_doi) '
-              . 'VALUE (?,?,?,?,?,?,?,?)',
+$dbh->do( 'INSERT IGNORE INTO study ' .
+            '(pi_name,pi_email,contact_name,contact_email,' .
+            'title,description,publication_date,ref_doi) ' .
+            'VALUE (?,?,?,?,?,?,?,?)',
           undef,
           @{$study}{
               'pi-name',          'pi-email',
@@ -89,40 +90,44 @@ $dbh->do( 'INSERT IGNORE INTO study '
 
 foreach my $dataset ( @{ $study->{'datasets'} } ) {
     die
-        if validate_required( $dataset, 'dataset',
-                qw( short-name full-name dataset-size version sample-sets ) );
+      if validate_required( $dataset, 'dataset',
+          qw( short-name full-name dataset-size version sample-sets ) );
 
     # Insert dataset
-    $dbh->do(
-        'INSERT IGNORE INTO dataset '
-            . '(study_pk,short_name,full_name,avg_seq_depth,'
-            . 'seq_type,seq_tech,seq_center,dataset_size,mongodb_collection) '
-            . 'SELECT study_pk,?,?,?,?,?,?,?,"non-existent" '
-            . 'FROM study WHERE title = ? AND pi_email = ?',
-        undef,
-        @{$dataset}{
-            'short-name', 'full-name',  'avg-seq-depth', 'seq-type',
-            'seq-tech',   'seq-center', 'dataset-size' },
-        @{$study}{ 'title', 'pi-email' } );
+    $dbh->do( 'INSERT IGNORE INTO dataset ' .
+                '(study_pk,short_name,full_name,avg_seq_depth,' .
+                'seq_type,seq_tech,seq_center,' .
+                'dataset_size,mongodb_collection) ' .
+                'SELECT study_pk,?,?,?,?,?,?,?,"non-existent" ' .
+                'FROM study WHERE title = ? AND pi_email = ?',
+              undef,
+              @{$dataset}{
+                  'short-name',    'full-name',
+                  'avg-seq-depth', 'seq-type',
+                  'seq-tech',      'seq-center',
+                  'dataset-size' },
+              @{$study}{ 'title', 'pi-email' } );
 
     my $version = $dataset->{'version'};
     die
-        if validate_required( $version, 'version',
-                              qw( version description terms ) );
+      if validate_required( $version, 'version',
+                            qw( version description terms ) );
 
     # Insert dataset_version
-    if ( exists( $version->{'description'} ) && -f $version->{'description'} )
+    if ( exists( $version->{'description'} ) &&
+         -f $version->{'description'} )
     {
-        $version->{'description'} = get_file( $version->{'description'} );
+        $version->{'description'} =
+          get_file( $version->{'description'} );
     }
     if ( exists( $version->{'terms'} ) && -f $version->{'terms'} ) {
         $version->{'terms'} = get_file( $version->{'terms'} );
     }
-    $dbh->do( 'INSERT IGNORE INTO dataset_version '
-                  . '(dataset_pk,version,description,terms,var_call_ref,'
-                  . 'available_from,ref_doi) '
-                  . 'SELECT dataset_pk,?,?,?,?,?,? '
-                  . 'FROM dataset WHERE short_name = ?',
+    $dbh->do( 'INSERT IGNORE INTO dataset_version ' .
+                '(dataset_pk,version,description,terms,var_call_ref,' .
+                'available_from,ref_doi) ' .
+                'SELECT dataset_pk,?,?,?,?,?,? ' .
+                'FROM dataset WHERE short_name = ?',
               undef,
               @{$version}{
                   'version',        'description',
@@ -133,36 +138,38 @@ foreach my $dataset ( @{ $study->{'datasets'} } ) {
     # Insert collection and sample_set
     foreach my $sample_set ( @{ $dataset->{'sample-sets'} } ) {
         die
-            if validate_required( $sample_set, 'sample-set',
-                                  qw( collection sample-size phenotype ) );
+          if validate_required( $sample_set, 'sample-set',
+                               qw( collection sample-size phenotype ) );
 
-        $dbh->do( 'INSERT IGNORE INTO collection '
-                      . '(name,ethnicity) VALUE (?,?)',
+        $dbh->do( 'INSERT IGNORE INTO collection ' .
+                    '(name,ethnicity) VALUE (?,?)',
                   undef,
                   @{$sample_set}{ 'collection', 'ethnicity' } );
-        $dbh->do( 'INSERT IGNORE INTO sample_set '
-                      . '(dataset_pk,collection_pk,sample_size,phenotype) '
-                      . 'SELECT d.dataset_pk,c.collection_pk,?,? '
-                      . 'FROM collection AS c JOIN dataset AS d '
-                      . 'WHERE c.name = ? AND d.short_name = ?',
-                  undef,
-                  @{$sample_set}{ 'sample-size', 'phenotype', 'collection' },
-                  $dataset->{'short-name'} );
+        $dbh->do('INSERT IGNORE INTO sample_set ' .
+                   '(dataset_pk,collection_pk,sample_size,phenotype) ' .
+                   'SELECT d.dataset_pk,c.collection_pk,?,? ' .
+                   'FROM collection AS c JOIN dataset AS d ' .
+                   'WHERE c.name = ? AND d.short_name = ?',
+                 undef,
+                 @{$sample_set}{ 'sample-size', 'phenotype',
+                     'collection' },
+                 $dataset->{'short-name'} );
     }
 
     # Insert logotype if present
-    if ( exists( $dataset->{'logotype'} ) && -f $dataset->{'logotype'} ) {
+    if ( exists( $dataset->{'logotype'} ) && -f $dataset->{'logotype'} )
+    {
         my $mt = MIME::Types->new();
-        $dataset->{'logotype-mimetype'}
-            = $mt->mimeTypeOf( $dataset->{'logotype'} )->type();
+        $dataset->{'logotype-mimetype'} =
+          $mt->mimeTypeOf( $dataset->{'logotype'} )->type();
         $dataset->{'logotype'} = get_file( $dataset->{'logotype'} );
 
-        $dbh->do( 'INSERT IGNORE INTO dataset_logo '
-                      . '(dataset_pk,data,mimetype) '
-                      . 'SELECT dataset_pk,?,? '
-                      . 'FROM dataset WHERE short_name = ?',
+        $dbh->do( 'INSERT IGNORE INTO dataset_logo ' .
+                    '(dataset_pk,data,mimetype) ' .
+                    'SELECT dataset_pk,?,? ' .
+                    'FROM dataset WHERE short_name = ?',
                   undef,
-                  @{$dataset}{ 'logotype', 'logotype-mimetype', 'short-name' }
-        );
+                  @{$dataset}{ 'logotype', 'logotype-mimetype',
+                      'short-name' } );
     }
 } ## end foreach my $dataset ( @{ $study...})
