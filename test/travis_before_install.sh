976eb0176af1d89875b71e1519594ec88ae2b0fc
Johan Viklund <johan.viklund@nbis.se>
2017-12-04 09:53:34 +0100

    Load datavolume into mysql

--- test/travis_before_install.sh
+++ test/travis_before_install.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+set -x
 
 docker pull mysql:5.7
-docker run --name mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -d -p 3366:3306  mysql:5.7
+docker pull ubuntu:16.04
+
+VOLUME='mysql-data-volume'
+MYSQL_PORT=3366
+
+test/docker-db/load_database_in_volume.sh
+docker run -v $VOLUME:/var/lib/mysql --rm --name mysql -d -p $MYSQL_PORT:3306 mysql:5.7
