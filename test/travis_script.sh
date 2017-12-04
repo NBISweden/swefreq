e24468dca9c82a5a083c6b0a181f05f34aa99557
Johan Viklund <johan.viklund@nbis.se>
2017-12-04 09:53:56 +0100

    Travis test runner

--- test/travis_script.sh
+++ test/travis_script.sh
@@ -1,10 +1,33 @@
 #!/usr/bin/env bash
+set -xe
 
 cp settings_sample.json settings.json
+sed -i 's/password//' settings.json
+sed -i 's/"mysqlSchema" : "swefreq"/"mysqlSchema" : "swefreq_test"/' settings.json
+sed -i 's/"mysqlPort" : 3306/"mysqlPort" : 3366/' settings.json
+
+echo "SETTINGS"
+cat settings.json
+echo "/SETTINGS"
+
+mysql -u swefreq -h 127.0.0.1 -P 3366 swefreq_test < sql/swefreq.sql
+mysql -u swefreq -h 127.0.0.1 -P 3366 swefreq_test < test/data/load_dummy_data.sql
 
 cd backend
-bash ../test/01_daemon_starts.sh
-cd ..
 
-mysql -u root -h 127.0.0.1 -P 3366 -e 'CREATE DATABASE swefreq;'
-mysql -u root -h 127.0.0.1 -P 3366 swefreq < sql/swefreq.sql
+../test/01_daemon_starts.sh
+
+python route.py --develop 1>http_log.txt 2>&1 &
+BACKEND_PID=$!
+
+function exit_handler() {
+    kill -9 $BACKEND_PID
+
+    echo "THE HTTP LOG WAS:"
+    cat http_log.txt
+}
+
+trap exit_handler EXIT
+
+sleep 2 # Lets wait a little bit so the server has started
+python test.py -v
