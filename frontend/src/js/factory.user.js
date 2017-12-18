acab902815821673da0ff753698c060c0d893afc
Johan Viklund <johan.viklund@nbis.se>
2017-12-18 16:07:40 +0100

    Added datasets to the user factory

--- frontend/src/js/factory.user.js
+++ frontend/src/js/factory.user.js
@@ -3,12 +3,21 @@
     .factory("User", ["$http", function($http) {
         return {
             getUser: getUser,
+            getDatasets: getDatasets,
         };
 
         function getUser() {
             return $http.get("/api/users/me")
                 .then(function(data) {
                     return data.data;
+                }
+            );
+        }
+
+        function getDatasets() {
+            return $http.get("/api/users/datasets")
+                .then(function(data) {
+                    return data.data.data;
                 }
             );
         }
