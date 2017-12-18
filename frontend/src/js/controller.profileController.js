a6436cc4862dbe15ed468bf9685a6c055cc3a9a2
Johan Viklund <johan.viklund@nbis.se>
2017-12-18 13:52:08 +0100

    Added profile controller

---
+++ frontend/src/js/controller.profileController.js
@@ -0,0 +1,28 @@
+(function() {
+    angular.module("App")
+    .controller("profileController", ["User", "Countries",
+                                function(User, Countries) {
+    var localThis                 = this;
+
+    activate();
+
+    function activate() {
+        User.getUser().then(function(data) {
+            localThis.user = data;
+            setCountry();
+        });
+
+        Countries.getCountries().then(function(data) {
+            localThis.availableCountries = data;
+            setCountry();
+        });
+    }
+
+    function setCountry() {
+        if (localThis.user && localThis.availableCountries) {
+            var country = localThis.user.country;
+            localThis.user.country = { name: country, id: country };
+        }
+    }
+    }]);
+})();
