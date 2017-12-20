NBISweden/swefreq

commit 69a06d807c6dd566465f5f2906751a75258b268e

Fix simple var errors

frontend/src/js/factory.datasetUsers.js
@@ -23,7 +23,7 @@
                         data["current"] = d.data.data;
                     }
                 )
-            ]).then(function(d) {
+            ]).then(function(data) {
                 defer.resolve(data);
             });
             return defer.promise;

frontend/src/js/controller.datasetAdminController.js
@@ -38,7 +38,7 @@
         function revokeUser(userData) {
             DatasetUsers.revokeUser(
                     $routeParams.dataset, userData.email
-                ).then(function(data) {
+                ).then(function() {
                     getUsers();
                 }
             );
@@ -47,7 +47,7 @@
         function approveUser(userData){
             DatasetUsers.approveUser(
                     $routeParams.dataset, userData.email
-                ).then(function(data) {
+                ).then(function() {
                     getUsers();
                 }
             );

frontend/src/js/controller.datasetBeaconController.js
@@ -38,7 +38,7 @@
                     d.response.state = d.response.exists ? "Present" : "Absent";
                     localThis.queryResponses.push(d);
                 },
-                function(response) {
+                function() {
                     localThis.queryResponses.push({
                         "response": { "state": "Error" },
                         "query": {

frontend/src/js/factory.beacon.js
@@ -10,7 +10,7 @@
             return $http.get("/api/beacon/info").then(function(data) {
                 var references = [];
                 var d = data.data.datasets;
-                console.log(d);
+
                 for (var i = 0; i < d.length; i++) {
                     var dataset = d[i];
                     if (dataset.id === name) {

frontend/src/js/factory.dataset.js
@@ -31,10 +31,10 @@
                     var contact_name = state.study.contact_name;
                     state.study.contact_name_uc = contact_name.charAt(0).toUpperCase() + contact_name.slice(1);
                 })
-            ]).then(function(data) {
+            ]).then(function() {
                     defer.resolve(state);
                 },
-                function(error) {
+                function() {
                     var error_message = "Can't find dataset " + dataset;
                     if (version) {
                         error_message += " version " + version;

frontend/src/js/factory.datasetList.js
@@ -7,7 +7,7 @@
 
         function getDatasetList() {
             return $http.get("/api/datasets").then(function(data) {
-                var data = data.data.data;
+                data = data.data.data;
                 var len = data.length;
                 var datasets = [];
 

frontend/src/js/factory.temporary_link.js
@@ -6,7 +6,7 @@
         };
 
         function getTemporary(dataset, version) {
-            url = "/api/datasets/" + dataset;
+            var url = "/api/datasets/" + dataset;
             if (version) {
                 url += "/versions/" + version;
             }

commit 063082861f39cd9198d50c06feb766bbe8e11bfd

Fixed bug introduced by 69a06d807

frontend/src/js/factory.datasetUsers.js
@@ -11,20 +11,20 @@
 
          function getUsers(dataset) {
             var defer = $q.defer();
-            var data = {"pending": [], "current": []};
+            var users = {"pending": [], "current": []};
             $q.all([
                 $http.get( "/api/datasets/" + dataset + "/users_pending" )
-                    .then(function(d) {
-                        data["pending"] = d.data.data;
+                    .then(function(data) {
+                        users.pending = data.data.data;
                     }
                 ),
                 $http.get( "/api/datasets/" + dataset + "/users_current" )
-                    .then(function(d) {
-                        data["current"] = d.data.data;
+                    .then(function(data) {
+                        users.current = data.data.data;
                     }
                 )
-            ]).then(function(data) {
-                defer.resolve(data);
+            ]).then(function() {
+                defer.resolve(users);
             });
             return defer.promise;
         }