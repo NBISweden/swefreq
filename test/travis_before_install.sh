5fb0a705a88db033d9d0c148a443fc8c09163135
Johan Viklund <johan.viklund@nbis.se>
2018-02-02 14:11:51 +0100

    Renamed scripts related to docker volume creation

--- test/travis_before_install.sh
+++ test/travis_before_install.sh
@@ -7,5 +7,5 @@ docker pull ubuntu:16.04
 VOLUME='mysql-data-volume'
 MYSQL_PORT=3366
 
-test/docker-db/load_database_in_volume.sh
+scripts/download_and_create_docker_db_volume.sh
 docker run -v $VOLUME:/var/lib/mysql --rm --name mysql -d -p $MYSQL_PORT:3306 mysql:5.7
