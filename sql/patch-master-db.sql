d336151e51b8b83aab2249a9bc9471cd0edabe6c
Johan Viklund <johan.viklund@nbis.se>
2018-04-05 17:00:17 +0200

    Reload the dataset_version_current view

--- sql/patch-master-db.sql
+++ sql/patch-master-db.sql
@@ -6,3 +6,10 @@ ALTER TABLE dataset_version
     ADD COLUMN (
     data_contact_name VARCHAR(100) DEFAULT NULL,
     data_contact_link VARCHAR(100) DEFAULT NULL);
+
+CREATE OR REPLACE VIEW dataset_version_current AS
+    SELECT * FROM dataset_version
+    WHERE (dataset_pk, dataset_version_pk) IN (
+        SELECT dataset_pk, MAX(dataset_version_pk) FROM dataset_version
+        WHERE available_from < now()
+        GROUP BY dataset_pk );
