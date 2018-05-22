10c6fa43ba50a8277a9477cc7a563f0777f18ca6
Johan Viklund <johan.viklund@nbis.se>
2018-05-22 16:27:45 +0200

    Correct allele frequency boxes

--- frontend/src/js/controller.browserController.js
+++ frontend/src/js/controller.browserController.js
@@ -37,7 +37,7 @@
         localThis.reorderVariants = reorderVariants;
 
 		// variant list frequency box thresholds
-		localThis.variantBoxThresholds = [0, 1/10000, 1/1000, 1/100, 1/20, 1/2, 1];
+		localThis.variantBoxThresholds = [0, 1/10000, 1/1000, 1/100, 1/20, 1/2];
 
         activate();
 
