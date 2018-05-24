fae3628272e686501df7be0d47fe856b8ee7aba5
Johan Viklund <johan.viklund@nbis.se>
2018-05-24 13:11:05 +0200

    Simpler formatting of variant id column in variant table

--- frontend/src/js/filter.shortSeq.js
+++ frontend/src/js/filter.shortSeq.js
@@ -1,12 +1,25 @@
 (function() {
     angular.module("App")
-    .filter('shortSeq', function() {
-        return function(input, len = 12) {
+    .filter('formatVariant', function() {
+        function shortenSeq(input, len=12) {
             var tail = Math.floor((len-5)/2)
             if (input.length >= len) {
                 return input.substr(0,tail) + "[...]" + input.substr(input.length-tail,input.length);
             }
             return input;
+        }
+
+        return function(variant, len = 12) {
+            var segments = variant['variantId'].split("-");
+            var text = "";
+            text += segments[0] + '-' + segments[1];
+            text += ' ' + shortenSeq(segments[2], len);
+            text += ' → ';
+            text += ' ' + shortenSeq(segments[3], len);
+            if ( variant['rsid'] ) {
+                text += " (" + variant['rsid'] + ")";
+            }
+            return text;
         };
     });
 })();
