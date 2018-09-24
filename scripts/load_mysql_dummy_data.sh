4b36cb4ea53f3408b32d502286cf50704a7272a1
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2018-09-24 11:50:42 +0200

    Break some long lines, change to /bin/sh, minor formatting

--- scripts/load_mysql_dummy_data.sh
+++ scripts/load_mysql_dummy_data.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 docker run -v mysql-data-volume:/var/lib/mysql --rm --name mysql -d mysql:5.7
 
-docker run -i --rm --link mysql:mysql mysql:5.7 mysql -h mysql -u swefreq swefreq_test < sql/swefreq.sql
-docker run -i --rm --link mysql:mysql mysql:5.7 mysql -h mysql -u swefreq swefreq_test < test/data/load_dummy_data.sql
+docker run -i --rm --link mysql:mysql mysql:5.7 mysql \
+    -h mysql -u swefreq swefreq_test <sql/swefreq.sql
+docker run -i --rm --link mysql:mysql mysql:5.7 mysql \
+    -h mysql -u swefreq swefreq_test <test/data/load_dummy_data.sql
 
 docker container stop mysql
