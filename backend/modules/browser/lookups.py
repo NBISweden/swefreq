dd0b5a580e2dad9c631031092f0e9368bc080849
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-01-08 16:10:50 +0100

    multiple updates to tests and code

--- backend/modules/browser/lookups.py
+++ backend/modules/browser/lookups.py
@@ -1,186 +1,19 @@
 import re
 import db
+import logging
 
 #from .utils import METRICS, AF_BUCKETS, get_xpos, xpos_to_pos, add_consequence_to_variants, add_consequence_to_variant
 
 SEARCH_LIMIT = 10000
 
 
-def get_coverage_for_bases(chrom, start_pos, stop_pos=None):
-    """
-    Get the coverage for the list of bases given by start_pos->xstop_pos, inclusive
-    Args:
-        chrom (str): chromosome
-        start_pos (int): first position of interest
-        end_pos (int): last position of interest; if None it will be set to start_pos
-    Returns:
-        list: coverage dicts for the region of interest
-    """
-    if stop_pos is None:
-        stop_pos = start_pos
-
-    return [values for values in db.Coverage.select().where((db.Coverage.pos >= start_pos) &
-                                                            (db.Coverage.pos <= stop_pos) &
-                                                            (db.Coverage.chrom == chrom)).dicts()]
-
-
-def get_coverage_for_transcript(chrom, start_pos, stop_pos=None):
-    """
-    Get the coverage for the list of bases given by start_pos->xstop_pos, inclusive
-    Args:
-        chrom (str): chromosome
-        start_pos (int): first position of interest
-        end_pos (int): last position of interest; if None it will be set to start_pos
-    Returns:
-        list: coverage dicts for the region of interest
-    """
-    # Is this function no longer relevant with postgres?
-    # Only entries with reported cov are in database
-    coverage_array = get_coverage_for_bases(chrom, start_pos, stop_pos)
-    # only return coverages that have coverage (if that makes any sense?)
-    # return coverage_array
-    covered = [c for c in coverage_array if c['mean']]
-    return covered
-
-
-def get_exons_in_transcript(transcript_dbid):
-    """
-    Retrieve exons associated with the given transcript id
-    Args:
-        transcript_dbid: the id of the transcript in the database (Transcript.id; not transcript_id)
-    Returns:
-        list: dicts with values for each exon sorted by start position
-    """
-    return sorted(list(db.Feature.select().where(db.Feature.transcript==transcript_dbid).dicts()), key=lambda k: k['start'])
-
-
-def get_gene(gene_id):
-    """
-    Retrieve gene by gene id
-    Args:
-        gene_id (str): the id of the gene
-    Returns:
-        dict: values for the gene; empty if not found
-    """
-    try:
-        return db.Gene.select().where(db.Gene.gene_id==gene_id).dicts().get()
-    except db.Gene.DoesNotExist:
-        return {}
-
-
-def get_gene_by_name(gene_name):
-    """
-    Retrieve gene by gene_name.
-    First checks gene_name, then other_names.
-    Args:
-        gene_name (str): the id of the gene
-    Returns:
-        dict: values for the gene; empty if not found
-    """
-    # try gene_name field first
-    try:
-        return db.Gene.select().where(db.Gene.name==gene_name).dicts().get()
-    except db.Gene.DoesNotExist:
-        try:
-            # troubles with KeyError
-            return db.Gene.select().where(db.Gene.other_names.contains(gene_name)).dicts().get()
-        except db.Gene.DoesNotExist:
-            return {}
-
-
-def get_transcript(transcript_id):
-    """
-    Retrieve transcript by transcript id
-    Also includes exons as ['exons']
-    Args:
-        transcript_id (str): the id of the transcript
-    Returns:
-        dict: values for the transcript, including exons; empty if not found
-    """
-    try:
-        transcript = db.Transcript.select().where(db.Transcript.transcript_id==transcript_id).dicts().get()
-        transcript['exons'] = get_exons_in_transcript(transcript['id'])
-        return transcript
-    except db.Transcript.DoesNotExist:
-        return {}
-
-
-def get_raw_variant(pos, chrom, ref, alt):
-    """
-    Retrieve variant by position and change
-    Args:
-        pos (int): position of the variant
-        chrom (str): name of the chromosome
-        ref (str): reference sequence
-        ref (str): variant sequence
-    Returns:
-        dict: values for the variant; empty if not found
-    """
-    try:
-        return db.Variant.select().where((db.Variant.pos == pos) &
-                                         (db.Variant.ref == ref) &
-                                         (db.Variant.alt == alt) &
-                                         (db.Variant.chrom == chrom)).dicts().get()
-    except db.Variant.DoesNotExist:
-        return {}
-
-
-def get_variant(pos, chrom, ref, alt):
-    """
-    Retrieve variant by position and change
-    Retrieves rsid from db (if available) if not present in variant
-    Args:
-        pos (int): position of the variant
-        chrom (str): name of the chromosome
-        ref (str): reference sequence
-        ref (str): variant sequence
-    Returns:
-        dict: values for the variant; empty if not found
-    """
-    try:
-        variant = get_raw_variant(pos, chrom, ref, alt)
-        if not variant or 'rsid' not in variant:
-            return variant
-        if variant['rsid'] == '.' or variant['rsid'] is None:
-            rsid = db.DbSNP.select().where((db.DbSNP.pos==pos) &
-                                           (db.DbSNP.chrom==chrom)).dicts().get()
-            if rsid:
-                variant['rsid'] = 'rs{}'.format(rsid['rsid'])
-        return variant
-    except db.Variant.DoesNotExist:
-        return {}
-
-
-def get_variants_in_transcript(transcript_id):
-    """
-    Retrieve variants inside a transcript
-    Args:
-        pos (int): position of the variant
-        chrom (str): name of the chromosome
-        ref (str): reference sequence
-        ref (str): variant sequence
-    Returns:
-        dict: values for the variant; empty if not found
-    """
-    variants = []
-    for variant in db.Variant.select().where(db.Variant.transcripts.contains(transcript_id)).dicts():
-        variants.append(variant)
-    return variants
-    variant['vep_annotations'] = [x for x in variant['vep_annotations'] if x['Feature'] == transcript_id]
-    add_consequence_to_variant(variant)
-    remove_extraneous_information(variant)
-    variants.append(variant)
-    return variants
-
-
 REGION_REGEX = re.compile(r'^\s*(\d+|X|Y|M|MT)\s*([-:]?)\s*(\d*)-?([\dACTG]*)-?([ACTG]*)')
 
-def get_awesomebar_result(db,sdb, query):
+def get_awesomebar_result(dataset, query):
     """
     Similar to the above, but this is after a user types enter
     We need to figure out what they meant - could be gene, variant, region
 
-    Return tuple of (datatype, identifier)
     Where datatype is one of 'gene', 'variant', or 'region'
     And identifier is one of:
     - ensembl ID for gene
@@ -192,10 +25,14 @@ def get_awesomebar_result(db,sdb, query):
     - if a gene symbol, return that gene's ensembl ID
     - if an RSID, return that variant's string
 
-
     Finally, note that we don't return the whole object here - only it's identifier.
     This could be important for performance later
 
+    Args:
+        dataset (str): short name of dataset
+        query (str): the search query
+    Returns:
+        tuple: (datatype, identifier)
     """
     query = query.strip()
 
@@ -248,20 +85,245 @@ def get_awesomebar_result(db,sdb, query):
     return 'not_found', query
 
 
-def get_genes_in_region(sdb, chrom, start, stop):
+def get_coverage_for_bases(dataset, chrom, start_pos, stop_pos=None):
     """
-    Genes that overlap a region
+    Get the coverage for the list of bases given by start_pos->xstop_pos, inclusive
+    Args:
+        chrom (str): chromosome
+        start_pos (int): first position of interest
+        end_pos (int): last position of interest; if None it will be set to start_pos
+    Returns:
+        list: coverage dicts for the region of interest
     """
-    xstart = get_xpos(chrom, start)
-    xstop = get_xpos(chrom, stop)
-    genes = sdb.genes.find({
-        'xstart': {'$lte': xstop},
-        'xstop': {'$gte': xstart},
-    }, projection={'_id': False})
-    return list(genes)
+    dataset_version = db.get_dataset_version(dataset)
+    print(dataset_version)
+    return dict(dataset_version)
+#    if stop_pos is None:
+#        stop_pos = start_pos
+
+#    return [values for values in db.Coverage.select().where((db.Coverage.pos >= start_pos) &
+#                                                            (db.Coverage.pos <= stop_pos) &
+#                                                            (db.Coverage.chrom == chrom) &
+#                                                            (db.Coverage.data)).dicts()]
 
 
-def get_variants_in_region(db, sdb, chrom, start, stop):
+def get_coverage_for_transcript(chrom, start_pos, stop_pos=None):
+    """
+    Get the coverage for the list of bases given by start_pos->xstop_pos, inclusive
+    Args:
+        chrom (str): chromosome
+        start_pos (int): first position of interest
+        end_pos (int): last position of interest; if None it will be set to start_pos
+    Returns:
+        list: coverage dicts for the region of interest
+    """
+    # Is this function still relevant with postgres?
+    # Only entries with reported cov are in database
+    coverage_array = get_coverage_for_bases(chrom, start_pos, stop_pos)
+    # only return coverages that have coverage (if that makes any sense?)
+    # return coverage_array
+    covered = [c for c in coverage_array if c['mean']]
+    return covered
+
+
+def get_exons_in_transcript(transcript_dbid):
+    """
+    Retrieve exons associated with the given transcript id
+    Args:
+        transcript_dbid: the id of the transcript in the database (Transcript.id; not transcript_id)
+    Returns:
+        list: dicts with values for each exon sorted by start position
+    """
+    return sorted(list(db.Feature.select().where((db.Feature.transcript==transcript_dbid) &
+                                                 (db.Feature.feature_type=='exon')).dicts()),
+                  key=lambda k: k['start'])
+
+
+def get_gene(dataset, gene_id):
+    """
+    Retrieve gene by gene id
+    Args:
+        dataset (str): short name of the dataset
+        gene_id (str): the id of the gene
+    Returns:
+        dict: values for the gene; empty if not found
+    """
+    ref_dbid = db.get_reference_dbid_dataset(dataset)
+    if not ref_dbid:
+        return {}
+    try:
+        return db.Gene.select().where((db.Gene.gene_id == gene_id) &
+                                      (db.Gene.reference_set == ref_dbid)).dicts().get()
+    except db.Gene.DoesNotExist:
+        return {}
+
+
+def get_gene_by_name(dataset, gene_name):
+    """
+    Retrieve gene by gene_name.
+    First checks gene_name, then other_names.
+    Args:
+        gene_name (str): the id of the gene
+    Returns:
+        dict: values for the gene; empty if not found
+    """
+    ref_dbid = db.get_reference_dbid_dataset(dataset)
+    if not ref_dbid:
+        return {}
+    try:
+        return db.Gene.select().where(db.Gene.name==gene_name).dicts().get()
+    except db.Gene.DoesNotExist:
+        try:
+            return db.Gene.select().where(db.Gene.other_names.contains(gene_name)).dicts().get()
+        except db.Gene.DoesNotExist:
+            return {}
+
+
+def get_genes_in_region(chrom, start_pos, stop_pos):
+    """
+    Retrieve genes located within a region
+    Args:
+        chrom (str): chromosome name
+        start_pos (int): start of region
+        stop_pos (int): end of region
+    Returns:
+        dict: values for the gene; empty if not found
+    """
+    gene_query = db.Gene.select().where((((db.Gene.start >= start_pos) &
+                                          (db.Gene.start <= stop_pos)) |
+                                         ((db.Gene.stop >= start_pos) &
+                                          (db.Gene.stop <= stop_pos))) &
+                                        (db.Gene.chrom == chrom)).dicts()
+    return [gene for gene in gene_query]
+
+
+def get_number_of_variants_in_transcript(db, transcript_id):
+    total = db.variants.count({'transcripts': transcript_id})
+    filtered = db.variants.count({'transcripts': transcript_id, 'filter': 'PASS'})
+    return {'filtered': filtered, 'total': total}
+
+
+def get_transcript(transcript_id):
+    """
+    Retrieve transcript by transcript id
+    Also includes exons as ['exons']
+    Args:
+        transcript_id (str): the id of the transcript
+    Returns:
+        dict: values for the transcript, including exons; empty if not found
+    """
+    try:
+        transcript = db.Transcript.select().where(db.Transcript.transcript_id==transcript_id).dicts().get()
+        transcript['exons'] = get_exons_in_transcript(transcript['id'])
+        return transcript
+    except db.Transcript.DoesNotExist:
+        return {}
+
+
+def get_raw_variant(pos, chrom, ref, alt):
+    """
+    Retrieve variant by position and change
+    Args:
+        pos (int): position of the variant
+        chrom (str): name of the chromosome
+        ref (str): reference sequence
+        ref (str): variant sequence
+    Returns:
+        dict: values for the variant; empty if not found
+    """
+    try:
+        return db.Variant.select().where((db.Variant.pos == pos) &
+                                         (db.Variant.ref == ref) &
+                                         (db.Variant.alt == alt) &
+                                         (db.Variant.chrom == chrom)).dicts().get()
+    except db.Variant.DoesNotExist:
+        return {}
+
+
+def get_transcripts_in_gene(dataset, gene_id):
+    """
+    Get the transcripts associated with a gene
+    Args:
+        dataset (str): short name of the reference set
+        gene_id (str): id of the gene
+    Returns:
+        list: transcripts (dict) associated with the gene
+    """
+    ref_dbid = db.get_reference_dbid_dataset(dataset)
+    gene = db.Gene.select().where((db.Gene.reference_set == ref_dbid) &
+                                  (db.Gene.gene_id == gene_id)).dicts().get()
+    return [transcript for transcript in db.Transcript.select().where(db.Transcript.gene == gene['id']).dicts()]
+
+
+def get_variant(pos, chrom, ref, alt):
+    """
+    Retrieve variant by position and change
+    Retrieves rsid from db (if available) if not present in variant
+    Args:
+        pos (int): position of the variant
+        chrom (str): name of the chromosome
+        ref (str): reference sequence
+        ref (str): variant sequence
+    Returns:
+        dict: values for the variant; empty if not found
+    """
+    try:
+        variant = get_raw_variant(pos, chrom, ref, alt)
+        if not variant or 'rsid' not in variant:
+            return variant
+        if variant['rsid'] == '.' or variant['rsid'] is None:
+            rsid = db.DbSNP.select().where((db.DbSNP.pos==pos) &
+                                           (db.DbSNP.chrom==chrom)).dicts().get()
+            if rsid:
+                variant['rsid'] = 'rs{}'.format(rsid['rsid'])
+        return variant
+    except db.Variant.DoesNotExist:
+        return {}
+
+
+def get_variants_in_gene(dataset, gene_id):
+    """
+    Retrieve variants present inside a gene
+    Args:
+        dataset: short name of the dataset
+        gene_id (str): id of the gene
+    Returns:
+        list: values for the variants
+    """
+    ref_dbid = db.get_reference_dbid_dataset(dataset)
+#    db.Variant.select().where(db.Variant.gene.contains(re
+    variants = []
+    for variant in db.variants.find({'genes': gene_id}, projection={'_id': False}):
+        variant['vep_annotations'] = [x for x in variant['vep_annotations'] if x['Gene'] == gene_id]
+        add_consequence_to_variant(variant)
+        remove_extraneous_information(variant)
+        variants.append(variant)
+    return variants
+
+
+def get_variants_in_transcript(transcript_id):
+    """
+    Retrieve variants inside a transcript
+    Args:
+        pos (int): position of the variant
+        chrom (str): name of the chromosome
+        ref (str): reference sequence
+        ref (str): variant sequence
+    Returns:
+        dict: values for the variant; empty if not found
+    """
+    variants = []
+    for variant in db.Variant.select().where(db.Variant.transcripts.contains(transcript_id)).dicts():
+        variants.append(variant)
+    return variants
+    variant['vep_annotations'] = [x for x in variant['vep_annotations'] if x['Feature'] == transcript_id]
+    add_consequence_to_variant(variant)
+    remove_extraneous_information(variant)
+    variants.append(variant)
+    return variants
+
+
+def get_variants_in_region(db, chrom, start, stop):
     """
     Variants that overlap a region
     Unclear if this will include CNVs
@@ -278,28 +340,6 @@ def get_variants_in_region(db, sdb, chrom, start, stop):
     return list(variants)
 
 
-def get_metrics(db, variant):
-    if 'allele_count' not in variant or variant['allele_num'] == 0:
-        return None
-    metrics = {}
-    for metric in METRICS:
-        metrics[metric] = db.metrics.find_one({'metric': metric}, projection={'_id': False})
-
-    metric = None
-    if variant['allele_count'] == 1:
-        metric = 'singleton'
-    elif variant['allele_count'] == 2:
-        metric = 'doubleton'
-    else:
-        for af in AF_BUCKETS:
-            if float(variant['allele_count'])/variant['allele_num'] < af:
-                metric = af
-                break
-    if metric is not None:
-        metrics['Site Quality'] = db.metrics.find_one({'metric': 'binned_%s' % metric}, projection={'_id': False})
-    return metrics
-
-
 def remove_extraneous_information(variant):
     #del variant['genotype_depths']
     #del variant['genotype_qualities']
@@ -311,24 +351,3 @@ def remove_extraneous_information(variant):
     del variant['xstop']
     del variant['site_quality']
     del variant['vep_annotations']
-
-
-def get_variants_in_gene(db, sdb, gene_id):
-    variants = []
-    for variant in db.variants.find({'genes': gene_id}, projection={'_id': False}):
-        variant['vep_annotations'] = [x for x in variant['vep_annotations'] if x['Gene'] == gene_id]
-        add_rsid_to_variant(sdb, variant)
-        add_consequence_to_variant(variant)
-        remove_extraneous_information(variant)
-        variants.append(variant)
-    return variants
-
-
-def get_transcripts_in_gene(sdb, gene_id):
-    return list(sdb.transcripts.find({'gene_id': gene_id}, projection={'_id': False}))
-
-
-def get_number_of_variants_in_transcript(db, transcript_id):
-    total = db.variants.count({'transcripts': transcript_id})
-    filtered = db.variants.count({'transcripts': transcript_id, 'filter': 'PASS'})
-    return {'filtered': filtered, 'total': total}
--- backend/modules/browser/test_lookups.py
+++ backend/modules/browser/test_lookups.py
@@ -1,15 +1,23 @@
-'''
+"""
 Tests for the functions available in lookups.py
-'''
+"""
 
 import lookups
 
 
+def test_get_awesomebar_result():
+    """
+    Test get_awesomebar_result()
+    """
+    pass
+
+
 def test_get_coverage_for_bases():
-    '''
+    """
     Test get_coverage_for_bases()
-    '''
-    coverage = lookups.get_coverage_for_bases('1', 55500283, 55500320)
+    """
+    coverage = lookups.get_coverage_for_bases('SweGen', '1', 55500283, 55500320)
+    print(type(coverage))
     expected = [{'id': 5474062, 'dataset_version': 4, 'chrom': '1',
                  'pos': 55500290, 'mean': 40.66, 'median': 39.0,
                  'coverage': [1.0, 1.0, 1.0, 1.0, 0.996, 0.97, 0.867, 0.127, 0.001]},
@@ -26,7 +34,7 @@ def test_get_coverage_for_bases():
 
 
 def test_get_coverage_for_transcript():
-    coverage = lookups.get_coverage_for_transcript('1', 55500283, 55500320)
+    # coverage = lookups.get_coverage_for_transcript('1', 55500283, 55500320)
     expected = [{'id': 5474062, 'dataset_version': 4, 'chrom': '1',
                  'pos': 55500290, 'mean': 40.66, 'median': 39.0,
                  'coverage': [1.0, 1.0, 1.0, 1.0, 0.996, 0.97, 0.867, 0.127, 0.001]},
@@ -43,30 +51,33 @@ def test_get_coverage_for_transcript():
 
 
 def test_get_exons_in_transcript():
-    '''
+    """
     Test get_exons_in_transcript()
-    '''
-    result = lookups.get_exons_in_transcript(5)
-    expected = [{'id': 28, 'gene': 2, 'transcript': 5, 'chrom': '1', 'start': 14364, 'stop': 14830, 'strand': '-', 'feature_type': 'exon'},
-                {'id': 27, 'gene': 2, 'transcript': 5, 'chrom': '1', 'start': 14971, 'stop': 15039, 'strand': '-', 'feature_type': 'exon'},
-                {'id': 26, 'gene': 2, 'transcript': 5, 'chrom': '1', 'start': 15797, 'stop': 15902, 'strand': '-', 'feature_type': 'exon'},
-                {'id': 25, 'gene': 2, 'transcript': 5, 'chrom': '1', 'start': 15905, 'stop': 15948, 'strand': '-', 'feature_type': 'exon'},
-                {'id': 24, 'gene': 2, 'transcript': 5, 'chrom': '1', 'start': 16608, 'stop': 16766, 'strand': '-', 'feature_type': 'exon'},
-                {'id': 23, 'gene': 2, 'transcript': 5, 'chrom': '1', 'start': 16855, 'stop': 17056, 'strand': '-', 'feature_type': 'exon'},
-                {'id': 22, 'gene': 2, 'transcript': 5, 'chrom': '1', 'start': 17234, 'stop': 17365, 'strand': '-', 'feature_type': 'exon'},
-                {'id': 21, 'gene': 2, 'transcript': 5, 'chrom': '1', 'start': 17603, 'stop': 17743, 'strand': '-', 'feature_type': 'exon'},
-                {'id': 20, 'gene': 2, 'transcript': 5, 'chrom': '1', 'start': 17916, 'stop': 18062, 'strand': '-', 'feature_type': 'exon'},
-                {'id': 19, 'gene': 2, 'transcript': 5, 'chrom': '1', 'start': 18269, 'stop': 18380, 'strand': '-', 'feature_type': 'exon'},
-                {'id': 18, 'gene': 2, 'transcript': 5, 'chrom': '1', 'start': 24739, 'stop': 24892, 'strand': '-', 'feature_type': 'exon'},
-                {'id': 17, 'gene': 2, 'transcript': 5, 'chrom': '1', 'start': 29322, 'stop': 29371, 'strand': '-', 'feature_type': 'exon'}]
-    print(result)
+    """
+    result = lookups.get_exons_in_transcript(28186)
+    expected = [{'id': 326403, 'gene': 8600, 'transcript': 28186, 'chrom': '2',
+                 'start': 202047893, 'stop': 202048032, 'strand': '+', 'feature_type': 'exon'},
+                {'id': 326404, 'gene': 8600, 'transcript': 28186, 'chrom': '2',
+                 'start': 202050495, 'stop': 202050848, 'strand': '+', 'feature_type': 'exon'},
+                {'id': 326406, 'gene': 8600, 'transcript': 28186, 'chrom': '2',
+                 'start': 202052430, 'stop': 202052523, 'strand': '+', 'feature_type': 'exon'},
+                {'id': 326408, 'gene': 8600, 'transcript': 28186, 'chrom': '2',
+                 'start': 202057708, 'stop': 202057843, 'strand': '+', 'feature_type': 'exon'},
+                {'id': 326410, 'gene': 8600, 'transcript': 28186, 'chrom': '2',
+                 'start': 202060566, 'stop': 202060672, 'strand': '+', 'feature_type': 'exon'},
+                {'id': 326412, 'gene': 8600, 'transcript': 28186, 'chrom': '2',
+                 'start': 202072799, 'stop': 202072907, 'strand': '+', 'feature_type': 'exon'},
+                {'id': 326414, 'gene': 8600, 'transcript': 28186, 'chrom': '2',
+                 'start': 202073794, 'stop': 202074286, 'strand': '+', 'feature_type': 'exon'},
+                {'id': 326416, 'gene': 8600, 'transcript': 28186, 'chrom': '2',
+                 'start': 202082312, 'stop': 202084804, 'strand': '+', 'feature_type': 'exon'}]
     assert result == expected
 
 
 def test_get_gene():
-    '''
+    """
     Test get_gene()
-    '''
+    """
     # normal entry
     expected = {'id': 1,
                 'reference_set': 1,
@@ -77,7 +88,7 @@ def test_get_gene():
                 'chrom': '1',
                 'start_pos': 11870,
                 'strand': '+'}
-    result = lookups.get_gene('ENSG00000223972')
+    result = lookups.get_gene('SweGen', 'ENSG00000223972')
     print(result)
     assert result['id'] == expected['id']
     assert result['reference_set'] == expected['reference_set']
@@ -89,15 +100,19 @@ def test_get_gene():
     assert result['start'] == expected['start_pos']
     assert result['strand'] == expected['strand']
 
-    # non-existing
-    result = lookups.get_gene('NOT_A_GENE')
+    # non-existing gene
+    result = lookups.get_gene('SweGen', 'NOT_A_GENE')
+    assert not result
+    
+    # non-existing dataset
+    result = lookups.get_gene('NoDataset', 'ENSG00000223972')
     assert not result
 
 
 def test_get_gene_by_name():
-    '''
+    """
     Test get_gene_by_name()
-    '''
+    """
     # normal entry
     expected = {'id': 1,
                 'reference_set': 1,
@@ -108,7 +123,7 @@ def test_get_gene_by_name():
                 'chrom': '1',
                 'start_pos': 11870,
                 'strand': '+'}
-    result = lookups.get_gene_by_name('DDX11L1')
+    result = lookups.get_gene_by_name('SweGen', 'DDX11L1')
     assert result['id'] == expected['id']
     assert result['reference_set'] == expected['reference_set']
     assert result['gene_id'] == expected['gene_id']
@@ -119,21 +134,33 @@ def test_get_gene_by_name():
     assert result['start'] == expected['start_pos']
     assert result['strand'] == expected['strand']
 
-    # non-exist
-    result = lookups.get_gene_by_name('NOT_A_GENE')
+    # non-existing gene
+    result = lookups.get_gene_by_name('SweGen', 'NOT_A_GENE')
+    assert not result
+    
+    # non-existing dataset
+    result = lookups.get_gene_by_name('NoDataset', 'ENSG00000223972')
     assert not result
 
-    # waiting for fixed db
-    result = lookups.get_gene_by_name('NOC2L')
+    # name in other_names
+    result = lookups.get_gene_by_name('SweGen', 'NIR')
     assert result['gene_id'] == 'ENSG00000188976'
-    result = lookups.get_gene_by_name('NIR')
-    result = lookups.get_gene_by_name('Z')
+    
+
+def test_get_genes_in_region():
+    """
+    Test get_genes_in_region()
+    """
+    res = lookups.get_genes_in_region('4', 99080000, 99210000)
+    # stop_pos missing in db, so needs to be updated when available
+    # exp_names = 
+    assert False
 
 
 def test_get_transcript():
-    '''
+    """
     Test get_transcript()
-    '''
+    """
     # normal entry
     expected = {'id': 5,
                 'transcript_id': 'ENST00000438504',
@@ -172,10 +199,24 @@ def test_get_transcript():
     assert not lookups.get_transcript('INCORRECT')
 
 
+def test_get_transcripts_in_gene():
+    """
+    Test get_transcripts_in_gene()
+    """
+    res = lookups.get_transcripts_in_gene('SweGen', 'ENSG00000241670')
+    expected = [{'id': 39, 'transcript_id': 'ENST00000424429', 'gene': 19,
+                 'mim_gene_accession': None, 'mim_annotation': None,
+                 'chrom': '1', 'start': 228293, 'stop': 228655, 'strand': '-'},
+                {'id': 40, 'transcript_id': 'ENST00000450734', 'gene': 19,
+                 'mim_gene_accession': None, 'mim_annotation': None,
+                 'chrom': '1', 'start': 228320, 'stop': 228776, 'strand': '-'}]
+    assert res == expected
+
+
 def test_get_raw_variant():
-    '''
+    """
     Test get_raw_variant
-    '''
+    """
     result = lookups.get_raw_variant(55500283, '1', 'A', 'T')
     assert result['genes'] == ['ENSG00000169174']
     assert result['transcripts'] == ['ENST00000302118']
@@ -183,9 +224,9 @@ def test_get_raw_variant():
 
 
 def test_get_variant():
-    '''
+    """
     Test get_variant()
-    '''
+    """
     result = lookups.get_variant(55500283, '1', 'A', 'T')
     assert result['genes'] == ['ENSG00000169174']
     assert result['transcripts'] == ['ENST00000302118']
@@ -196,8 +237,9 @@ def test_get_variant():
 
 
 def test_get_variants_in_transcript():
-    '''
+    """
     Test get_variants_in_transcript()
-    '''
-    res = lookups.get_variants_in_transcript('ENST00000302118')
-    assert len(res) == 426
+    """
+    # res = lookups.get_variants_in_transcript('ENST00000302118')
+    #  assert len(res) == 426
+    assert False
