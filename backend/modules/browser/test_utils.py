914442abe5fba71ae833f9db3f9d2079a53f7e1c
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-01-23 13:36:33 +0100

    adding tests and documentation

--- backend/modules/browser/test_utils.py
+++ backend/modules/browser/test_utils.py
@@ -12,31 +12,89 @@ def test_add_consequence_to_variants():
     """
     Test add_consequence_to_variants()
     """
-    assert False
+    variants = []
+    variants.append(lookups.get_variant('SweGen', 47730411, '21', 'TA', 'T'))
+    variants.append(lookups.get_variant('SweGen', 55500283, '1', 'A', 'T'))
+    variants[0]['vep_annotations'] = json.loads(variants[0]['vep_annotations']) # remove when db is fixed
+    variants[1]['vep_annotations'] = json.loads(variants[1]['vep_annotations']) # remove when db is fixed
+
+    utils.add_consequence_to_variants(variants)
+    assert variants[0]['major_consequence'] == 'intron_variant'
+    assert variants[1]['major_consequence'] == 'upstream_gene_variant'
 
 
 def test_add_consequence_to_variant():
     """
     Test add_consequence_to_variant()
     """
-    # variant = lookups.get_variant('SweGen', 47730411, '21', 'TA', 'T')
-    variant2 = lookups.get_variant('SweGen', 55500283, '1', 'A', 'T')
-    # variant2['vep_annotations'] = 
-    result = utils.add_consequence_to_variant(variant2)
-    # result = utils.add_consequence_to_variant(variant)
-    print(result)
-    print(result['major_consequence'])
-    print(result['category'])
+    variant = lookups.get_variant('SweGen', 47730411, '21', 'TA', 'T')
+    variant['vep_annotations'] = json.loads(variant['vep_annotations']) # remove when db is fixed
+    utils.add_consequence_to_variant(variant)
+    assert variant['major_consequence'] == 'intron_variant'
     
-    assert False
-
+    variant2 = lookups.get_variant('SweGen', 55500283, '1', 'A', 'T')
+    variant2['vep_annotations'] = json.loads(variant2['vep_annotations']) # remove when db is fixed
+    utils.add_consequence_to_variant(variant2)
+    assert variant2['major_consequence'] == 'upstream_gene_variant'
+    
 
 def test_annotation_severity():
     """
     Test annotation_severity()
     """
     variant = lookups.get_variant('SweGen', 55500283, '1', 'A', 'T')
-    utils.annotation_severity(variant['vep_annotations'])
+    variant['vep_annotations'] = json.loads(variant['vep_annotations']) # remove when db is fixed
+    res = utils.annotation_severity(variant['vep_annotations'][0])
+    assert res == -26.9
+    
+
+def test_get_flags_from_variant():
+    """
+    Test get_flags_from_variant()
+    """
+    assert False
+
+
+def test_get_minimal_representation():
+    """
+    Test get_minimal_representation()
+    """
+    assert False
+
+
+def test_get_proper_hgvs():
+    """
+    Test get_proper_hgvs()
+    """
+    assert False
+
+
+def test_get_protein_hgvs():
+    """
+    Test get_protein_hgvs()
+    """
+    assert False
+
+
+def test_get_transcript_hgvs():
+    """
+    Test get_transcript_hgvs()
+    """
+    assert False
+
+
+def test_order_vep_by_csq():
+    """
+    Test order_vep_by_csq()
+    """
+    assert False
+
+
+def test_remove_extraneous_vep_annotations():
+    """
+    Test remove_extraneous_vep_annotations()
+    """
+    assert False
 
 
 def test_worst_csq_from_csq():
@@ -44,9 +102,44 @@ def test_worst_csq_from_csq():
     Test worst_csq_from_csq()
     """
     variant = lookups.get_variant('SweGen', 55500283, '1', 'A', 'T')
-    print(type(variant['vep_annotations']))
-    print(variant['vep_annotations'])
-    vep = json.loads(variant['vep_annotations'])[0]
-    print(vep['Consequence'])
-    utils.worst_csq_from_csq(vep['Consequence'])
+    variant['vep_annotations'] = json.loads(variant['vep_annotations']) # remove when db is fixed
+    res = utils.worst_csq_from_csq(variant['vep_annotations'][0]['Consequence'])
+    assert res == 'upstream_gene_variant'
+    res = utils.worst_csq_from_csq('non_coding_exon_variant&nc_transcript_variant')
+    assert res == 'non_coding_exon_variant'
+
+
+def test_worst_csq_from_list():
+    """
+    Test worst_csq_from_list()
+    """
     assert False
+
+
+def test_worst_csq_index():
+    """
+    Test worst_csq_index()
+    """
+    csqs = ['frameshift_variant', 'missense_variant']
+    assert utils.worst_csq_index(csqs) == 4
+
+
+def test_worst_csq_with_vep():
+    """
+    Test worst_csq_from_vep()
+    """
+    veps = [{'SYMBOL': '1', 'Consequence': 'intergenic_variant', 'CANONICAL': ''},
+            {'SYMBOL': '2', 'Consequence': 'frameshift_variant', 'CANONICAL': ''},
+            {'SYMBOL': '3', 'Consequence': 'intron_variant', 'CANONICAL': ''},
+            {'SYMBOL': '4', 'Consequence': 'stop_lost', 'CANONICAL': ''}]
+    res = utils.worst_csq_with_vep(veps)
+    assert res == {'SYMBOL': '2', 'Consequence': 'frameshift_variant',
+                   'CANONICAL': '', 'major_consequence': 'frameshift_variant'}
+
+    veps = [{'SYMBOL': '1', 'Consequence': 'frameshift_variant', 'CANONICAL': 'YES'},
+            {'SYMBOL': '2', 'Consequence': 'frameshift_variant', 'CANONICAL': ''},
+            {'SYMBOL': '3', 'Consequence': 'intron_variant', 'CANONICAL': ''},
+            {'SYMBOL': '4', 'Consequence': 'stop_lost', 'CANONICAL': ''}]
+    res = utils.worst_csq_with_vep(veps)
+    assert res == {'SYMBOL': '1', 'Consequence': 'frameshift_variant',
+                   'CANONICAL': 'YES', 'major_consequence': 'frameshift_variant'}
--- backend/modules/browser/utils.py
+++ backend/modules/browser/utils.py
@@ -2,20 +2,13 @@ import logging
 from operator import itemgetter
 
 AF_BUCKETS = [0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1]
-METRICS = [
-    'BaseQRankSum',
-    'ClippingRankSum',
-    'DP',
-    'FS',
-    'InbreedingCoeff',
-    'MQ',
-    'MQRankSum',
-    'QD',
-    'ReadPosRankSum',
-    'VQSLOD'
-]
+
+CHROMOSOMES = ['chr%s' % x for x in range(1, 23)]
+CHROMOSOMES.extend(['chrX', 'chrY', 'chrM'])
+CHROMOSOME_TO_CODE = { item: i+1 for i, item in enumerate(CHROMOSOMES) }
 
 # Note that this is the current as of v81 with some included for backwards compatibility (VEP <= 75)
+
 CSQ_ORDER = ["transcript_ablation",
 "splice_acceptor_variant",
 "splice_donor_variant",
@@ -61,10 +54,33 @@ CSQ_ORDER_DICT = {csq:i for i,csq in enumerate(CSQ_ORDER)}
 REV_CSQ_ORDER_DICT = dict(enumerate(CSQ_ORDER))
 assert all(csq == REV_CSQ_ORDER_DICT[CSQ_ORDER_DICT[csq]] for csq in CSQ_ORDER)
 
+METRICS = [
+    'BaseQRankSum',
+    'ClippingRankSum',
+    'DP',
+    'FS',
+    'InbreedingCoeff',
+    'MQ',
+    'MQRankSum',
+    'QD',
+    'ReadPosRankSum',
+    'VQSLOD'
+]
+
+PROTEIN_LETTERS_1TO3 = {
+    'A': 'Ala', 'C': 'Cys', 'D': 'Asp', 'E': 'Glu',
+    'F': 'Phe', 'G': 'Gly', 'H': 'His', 'I': 'Ile',
+    'K': 'Lys', 'L': 'Leu', 'M': 'Met', 'N': 'Asn',
+    'P': 'Pro', 'Q': 'Gln', 'R': 'Arg', 'S': 'Ser',
+    'T': 'Thr', 'V': 'Val', 'W': 'Trp', 'Y': 'Tyr',
+    'X': 'Ter', '*': 'Ter', 'U': 'Sec'
+}
+
 
 def add_consequence_to_variants(variant_list):
     """
     Add information about variant consequence to multiple variants
+
     Args:
         variant_list (list): list of variants
     """
@@ -75,6 +91,7 @@ def add_consequence_to_variants(variant_list):
 def add_consequence_to_variant(variant):
     """
     Add information about variant consequence to a variant
+
     Args:
         variant (dict): variant information
     """
@@ -105,7 +122,35 @@ def add_consequence_to_variant(variant):
     variant['flags'] = get_flags_from_variant(variant)
 
 
+def annotation_severity(annotation):
+    """
+    Evaluate severity of the consequences; "bigger is more important"
+
+    Args:
+        annotation (dict): vep_annotation from a variant
+
+    Returns:
+        float: severity score
+    """
+    rv = -CSQ_ORDER_DICT[worst_csq_from_csq(annotation['Consequence'])]
+    if annotation['CANONICAL'] == 'YES':
+        rv += 0.1
+    return rv
+
+
 def get_flags_from_variant(variant):
+    """
+    Get flags from variant.
+    checks for: 
+     - MNP (identical length of reference and variant)
+     - LoF (loss of function)
+
+    Args:
+        variant (dict): a variant
+
+    Returns:
+        list: flags for the variant
+    """
     flags = []
     if 'mnps' in variant:
         flags.append('MNP')
@@ -117,106 +162,6 @@ def get_flags_from_variant(variant):
     if all([x['LoF_flags'] != '' for x in lof_annotations]):
         flags.append('LoF flag')
     return flags
-
-
-protein_letters_1to3 = {
-    'A': 'Ala', 'C': 'Cys', 'D': 'Asp', 'E': 'Glu',
-    'F': 'Phe', 'G': 'Gly', 'H': 'His', 'I': 'Ile',
-    'K': 'Lys', 'L': 'Leu', 'M': 'Met', 'N': 'Asn',
-    'P': 'Pro', 'Q': 'Gln', 'R': 'Arg', 'S': 'Ser',
-    'T': 'Thr', 'V': 'Val', 'W': 'Trp', 'Y': 'Tyr',
-    'X': 'Ter', '*': 'Ter', 'U': 'Sec'
-}
-
-
-def get_proper_hgvs(csq):
-    # Needs major_consequence
-    if csq['major_consequence'] in ('splice_donor_variant', 'splice_acceptor_variant', 'splice_region_variant'):
-        return get_transcript_hgvs(csq)
-
-    return get_protein_hgvs(csq)
-
-
-def get_transcript_hgvs(csq):
-    return csq['HGVSc'].split(':')[-1]
-
-
-def get_protein_hgvs(annotation):
-    """
-    Takes consequence dictionary, returns proper variant formatting for synonymous variants
-    """
-    if '%3D' in annotation['HGVSp']: # "%3D" is "="
-        try:
-            amino_acids = ''.join([protein_letters_1to3[x] for x in annotation['Amino_acids']])
-            return "p." + amino_acids + annotation['Protein_position'] + amino_acids
-        except KeyError:
-            logging.error("Could not fetch protein hgvs - unknown amino acid")
-    return annotation['HGVSp'].split(':')[-1]
-
-
-def remove_extraneous_vep_annotations(annotation_list):
-    return [ann for ann in annotation_list if worst_csq_index(ann['Consequence'].split('&')) <= CSQ_ORDER_DICT['intron_variant']]
-
-
-def worst_csq_index(csq_list):
-    """
-    Input list of consequences (e.g. ['frameshift_variant', 'missense_variant'])
-    Return index of the worst consequence (In this case, index of 'frameshift_variant', so 4)
-    Works well with worst_csq_index('non_coding_exon_variant&nc_transcript_variant'.split('&'))
-    """
-    return min([CSQ_ORDER_DICT[csq] for csq in csq_list])
-
-
-def worst_csq_from_list(csq_list):
-    """
-    Input list of consequences (e.g. ['frameshift_variant', 'missense_variant'])
-    Return the worst consequence (In this case, 'frameshift_variant')
-    Works well with worst_csq_from_list('non_coding_exon_variant&nc_transcript_variant'.split('&'))
-    """
-    return REV_CSQ_ORDER_DICT[worst_csq_index(csq_list)]
-
-
-def worst_csq_from_csq(csq):
-    """
-    Input possibly &-filled csq string (e.g. 'non_coding_exon_variant&nc_transcript_variant')
-    Return the worst consequence (In this case, 'non_coding_exon_variant')
-    """
-    return REV_CSQ_ORDER_DICT[worst_csq_index(csq.split('&'))]
-
-
-def order_vep_by_csq(annotation_list):
-    """
-    Adds "major_consequence" to each annotation.
-    Returns them ordered from most deleterious to least.
-    """
-    for ann in annotation_list:
-        ann['major_consequence'] = worst_csq_from_csq(ann['Consequence'])
-    return sorted(annotation_list, key=(lambda ann:CSQ_ORDER_DICT[ann['major_consequence']]))
-
-
-def worst_csq_with_vep(annotation_list):
-    """
-    Takes list of VEP annotations [{'Consequence': 'frameshift', Feature: 'ENST'}, ...]
-    Returns most severe annotation (as full VEP annotation [{'Consequence': 'frameshift', Feature: 'ENST'}])
-    Also tacks on "major_consequence" for that annotation (i.e. worst_csq_from_csq)
-    """
-    if not annotation_list:
-        return None
-    worst = max(annotation_list, key=annotation_severity)
-    worst['major_consequence'] = worst_csq_from_csq(worst['Consequence'])
-    return worst
-
-
-def annotation_severity(annotation):
-    "Bigger is more important."
-    rv = -CSQ_ORDER_DICT[worst_csq_from_csq(annotation['Consequence'])]
-    if annotation['CANONICAL'] == 'YES':
-        rv += 0.1
-    return rv
-
-CHROMOSOMES = ['chr%s' % x for x in range(1, 23)]
-CHROMOSOMES.extend(['chrX', 'chrY', 'chrM'])
-CHROMOSOME_TO_CODE = { item: i+1 for i, item in enumerate(CHROMOSOMES) }
 
 
 def get_minimal_representation(pos, ref, alt):
@@ -248,3 +193,95 @@ def get_minimal_representation(pos, ref, alt):
         ref = ref[1:]
         pos += 1
     return pos, ref, alt
+
+
+def get_proper_hgvs(csq):
+    # Needs major_consequence
+    if csq['major_consequence'] in ('splice_donor_variant', 'splice_acceptor_variant', 'splice_region_variant'):
+        return get_transcript_hgvs(csq)
+
+    return get_protein_hgvs(csq)
+
+
+def get_protein_hgvs(annotation):
+    """
+    Takes consequence dictionary, returns proper variant formatting for synonymous variants
+    """
+    if '%3D' in annotation['HGVSp']: # "%3D" is "="
+        try:
+            amino_acids = ''.join([PROTEIN_LETTERS_1TO3[x] for x in annotation['Amino_acids']])
+            return "p." + amino_acids + annotation['Protein_position'] + amino_acids
+        except KeyError:
+            logging.error("Could not fetch protein hgvs - unknown amino acid")
+    return annotation['HGVSp'].split(':')[-1]
+
+
+def get_transcript_hgvs(csq):
+    return csq['HGVSc'].split(':')[-1]
+
+
+def order_vep_by_csq(annotation_list):
+    """
+    Adds "major_consequence" to each annotation.
+    Returns them ordered from most deleterious to least.
+    """
+    for ann in annotation_list:
+        ann['major_consequence'] = worst_csq_from_csq(ann['Consequence'])
+    return sorted(annotation_list, key=(lambda ann:CSQ_ORDER_DICT[ann['major_consequence']]))
+
+
+def remove_extraneous_vep_annotations(annotation_list):
+    return [ann for ann in annotation_list if worst_csq_index(ann['Consequence'].split('&')) <= CSQ_ORDER_DICT['intron_variant']]
+
+
+def worst_csq_from_list(csq_list):
+    """
+    Input list of consequences (e.g. ['frameshift_variant', 'missense_variant'])
+    Return the worst consequence (In this case, 'frameshift_variant')
+    Works well with worst_csq_from_list('non_coding_exon_variant&nc_transcript_variant'.split('&'))
+    """
+    return REV_CSQ_ORDER_DICT[worst_csq_index(csq_list)]
+
+
+def worst_csq_from_csq(csq):
+    """
+    Find worst consequence in a possibly &-filled consequence string 
+
+    Args:
+        csq (str): string of consequences, seperated with & (if multiple)
+    
+    Returns:
+        str: the worst consequence
+    """
+    return REV_CSQ_ORDER_DICT[worst_csq_index(csq.split('&'))]
+
+
+def worst_csq_index(csq_list):
+    """
+    Find the index of the worst consequence.
+    Corresponds to the lowest value (index) from CSQ_ORDER_DICT
+
+    Args:
+        csq_list (list): consequences
+
+    Returns:
+        int: index in CSQ_ODER_DICT of the worst consequence
+    """
+    return min([CSQ_ORDER_DICT[csq] for csq in csq_list])
+
+
+def worst_csq_with_vep(annotation_list):
+    """
+    Choose the vep annotation with the most severe consequence
+
+    Args:
+        annotation_list (list): VEP annotations 
+    
+    Returns:
+        dict: the annotation with the most severe consequence; also adds "major_consequence" for that annotation
+    """
+    if not annotation_list:
+        return None
+    worst = max(annotation_list, key=annotation_severity)
+    worst['major_consequence'] = worst_csq_from_csq(worst['Consequence'])
+    return worst
