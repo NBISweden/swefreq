05a60e22e065153cc2c0e741634187939e501f7d
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2019-03-14 16:53:40 +0100

    Add missing quotes and make existing quotes consistent

--- test/travis_before_install.sh
+++ test/travis_before_install.sh
@@ -3,6 +3,6 @@
 PSQL_VERSION="10"
 PSQL_PORT="5433"
 
-docker pull postgres:"${PSQL_VERSION}"
+docker pull "postgres:$PSQL_VERSION"
 
-docker run --rm -d -p $PSQL_PORT:5432 postgres:"${PSQL_VERSION}"
+docker run --rm -d -p "$PSQL_PORT:5432" "postgres:$PSQL_VERSION"
