edb38bdb710d3180709a9e896d64040130e26560
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-04-18 21:08:45 +0200

    return after send_error

--- backend/handlers.py
+++ backend/handlers.py
@@ -133,10 +133,12 @@ class AuthorizedHandler(SafeHandler):
         if not 'dataset' in kwargs:
             logging.debug("No dataset: Send error 403")
             self.send_error(status_code=403)
+            return
         ds_version = kwargs['ds_version'] if 'ds_version' in kwargs else None
         if not self.current_user.has_access(db.get_dataset(kwargs['dataset']), ds_version):
             logging.debug("No user access: Send error 403")
             self.send_error(status_code=403)
+            return
         logging.debug("User is authorized")
 
 
