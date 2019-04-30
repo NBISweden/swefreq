de997c507434a9e2d86a8ffa28442a35bc37e19c
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-04-30 10:24:06 +0200

    Make beacon searches specific to dataset and remove reference selection from the the frontend.

--- frontend/src/js/controller.datasetBeaconController.js
+++ frontend/src/js/controller.datasetBeaconController.js
@@ -12,7 +12,7 @@
             Beacon.getBeaconReferences($routeParams.dataset, $routeParams.version)
 		.then(function(data) {
 		    if (data) {
-			localThis.references = data;
+			localThis.beaconInfo = data;
 		    }
                 });
 
@@ -33,7 +33,7 @@
         function search() {
             Beacon.queryBeacon(localThis)
                 .then(function(response) {
-		    if (!response.data.datasetAlleleResponses[0]) {
+		    if (response.data.exists===false) { // value may be null -> error
 			localThis.queryResponses.push({
                             "response": { "state": "Absent" },
                             "query": {
@@ -41,23 +41,32 @@
 				"position":        localThis.position,
 				"allele":          localThis.allele,
 				"referenceAllele": localThis.referenceAllele,
-				"reference":       localThis.reference
+                            }
+			});
+		    }
+		    else if (response.data.exists===true) {
+			localThis.queryResponses.push({
+			    "response": { "state": "Present" },
+			    "query": {
+				"chromosome":      localThis.chromosome,
+				"position":        localThis.position,
+				"allele":          localThis.allele,
+				"referenceAllele": localThis.referenceAllele,
                             }
 			});
 		    }
 		    else {
 			localThis.queryResponses.push({
-			    "response": { "state": "Present" },
-			    "query": {
-				"chromosome": response.data.datasetAlleleResponses[0].referenceName,
-				"position": response.data.datasetAlleleResponses[0].start + 1, // Beacon is 0-based
-				"allele": response.data.datasetAlleleResponses[0].alternateBases,
-				"referenceAllele": response.data.datasetAlleleResponses[0].referenceBases,
-				"reference": response.data.datasetAlleleResponses[0].datasetId.substring(0, 6),
-			    }
+                            "response": { "state": "Error" },
+                            "query": {
+				"chromosome":      localThis.chromosome,
+				"position":        localThis.position,
+				"allele":          localThis.allele,
+				"referenceAllele": localThis.referenceAllele,
+                            }
 			});
-		    }
-                },
+                    }
+		},
                 function() {
                     localThis.queryResponses.push({
                         "response": { "state": "Error" },
@@ -66,7 +75,6 @@
                             "position":        localThis.position,
                             "allele":          localThis.allele,
                             "referenceAllele": localThis.referenceAllele,
-                            "reference":       localThis.reference
                         }
                     });
                 }
