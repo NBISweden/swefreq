e0180a0859f7b5d38057eb4819f482f9c880e27b
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-08-15 10:18:53 +0200

    Remove check as the situation will never occur.
    If get_transcripts can't find the transcript, it will raise an exception and not return anything.

--- backend/modules/browser/utils.py
+++ backend/modules/browser/utils.py
@@ -360,8 +360,6 @@ def get_variant_list(dataset: str, datatype: str, item: str, ds_version: str = N
     elif datatype == 'transcript':
         variants = lookups.get_variants_in_transcript(dataset, item, ds_version)
         transcript = lookups.get_transcript(dataset, item, ds_version)
-        if not transcript:
-            return {}
         refgene = transcript['gene_id']
 
     if variants:
