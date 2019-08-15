573a1c7b3380d7348d46bf500aab8d2a22ef131a
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-08-15 08:48:13 +0200

    Fix whitespace.

--- backend/modules/browser/route.py
+++ backend/modules/browser/route.py
@@ -3,14 +3,13 @@ from . import browser_handlers as handlers
 # pylint: disable=line-too-long
 
 # Browser links
-routes = [(r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/gene/(?P<gene>[^/]+)" ,                              handlers.GetGene),
-          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/region/(?P<region>[^\/]+)",                          handlers.GetRegion),
-          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/transcript/(?P<transcript>[^/]+)",                   handlers.GetTranscript),
-          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/variant/(?P<variant>[^/]+)",                         handlers.GetVariant),
-          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/variants/(?P<datatype>[^/]+)/(?P<item>[^/]+)",       handlers.GetVariants),
-          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/coverage/(?P<datatype>[^/]+)/(?P<item>[^/]+)",       handlers.GetCoverage),
-          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/coverage_pos/(?P<datatype>[^/]+)/(?P<item>[^/]+)",   handlers.GetCoveragePos),
-          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/autocomplete/(?P<query>[^/]+)",                      handlers.Autocomplete),
-          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/search/(?P<query>[^/]+)",                            handlers.Search),
-          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/download/(?P<datatype>[^/]+)/(?P<item>[^/]+)(?:/filter/(?P<filter_type>[^/]+))?",       handlers.Download),
-        ]
+routes = [(r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/gene/(?P<gene>[^/]+)", handlers.GetGene),
+          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/region/(?P<region>[^\/]+)", handlers.GetRegion),
+          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/transcript/(?P<transcript>[^/]+)", handlers.GetTranscript),
+          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/variant/(?P<variant>[^/]+)", handlers.GetVariant),
+          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/variants/(?P<datatype>[^/]+)/(?P<item>[^/]+)", handlers.GetVariants),
+          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/coverage/(?P<datatype>[^/]+)/(?P<item>[^/]+)", handlers.GetCoverage),
+          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/coverage_pos/(?P<datatype>[^/]+)/(?P<item>[^/]+)", handlers.GetCoveragePos),
+          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/autocomplete/(?P<query>[^/]+)", handlers.Autocomplete),
+          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/search/(?P<query>[^/]+)", handlers.Search),
+          (r"/api/dataset/(?P<dataset>[^/]+)/(?:version/(?P<ds_version>[^/]+)/)?browser/download/(?P<datatype>[^/]+)/(?P<item>[^/]+)(?:/filter/(?P<filter_type>[^/]+))?", handlers.Download)]
