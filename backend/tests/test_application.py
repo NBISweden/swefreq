c7bfa94f7fe83970d8e81687c24f83f0f95b9355
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-08-21 14:16:51 +0200

    Test GetDataset.

--- backend/tests/test_application.py
+++ backend/tests/test_application.py
@@ -33,6 +33,19 @@ def test_get_schema():
                 "citation": "doi"}
     assert data['dataset'] == expected
 
+    response = requests.get(f'{BASE_URL}/api/schema?url={BASE_URL}/dataset/{ds_name}/version/123456/browser')
+    assert not response.text
+    assert response.status_code == 404
+
+    response = requests.get(f'{BASE_URL}/api/schema?url={BASE_URL}/dataset/bad_ds_name/browser')
+    assert not response.text
+    assert response.status_code == 404
+
+    ds_name = 'SweGen2'
+    response = requests.get(f'{BASE_URL}/api/schema?url={BASE_URL}/dataset/{ds_name}/version/UNRELEASED/browser')
+    assert not response.text
+    assert response.status_code == 403
+
     response = requests.get(f'{BASE_URL}/api/schema?url={BASE_URL}/dataset/{ds_name}/beacon')
     data = json.loads(response.text)
     expected = {'@id': 'https://swefreq.nbis.se/api/beacon-elixir/',
@@ -60,3 +73,66 @@ def test_get_countrylist():
     data = json.loads(response.text)
 
     assert len(data['countries']) == 240
+
+
+def test_get_dataset():
+    """
+    Test GetDataset.get()
+    """
+    ds_name = 'SweGen'
+    response = requests.get(f'{BASE_URL}/api/dataset/{ds_name}')
+    data = json.loads(response.text)
+    expected = {"study": 1,
+                "shortName": "SweGen",
+                "fullName": "SweGen",
+                "version": {"version": "20180409",
+                            "description": "desc",
+                            "terms": "terms",
+                            "availableFrom": "2001-01-04",
+                            "refDoi": "doi",
+                            "dataContactName": "place",
+                            "dataContactLink": "email",
+                            "numVariants": None,
+                            "coverageLevels": [1, 5, 10, 15, 20, 25, 30, 50, 100],
+                            "portalAvail": True,
+                            "fileAccess": "REGISTERED",
+                            "beaconAccess": "PUBLIC",
+                            "dataset": 1,
+                            "referenceSet": 1,
+                            "varCallRef": None},
+                "future": False}
+    for value in expected:
+        assert data[value] == expected[value]
+    assert len(data) == 14
+
+    ds_name = 'SweGen2'
+    response = requests.get(f'{BASE_URL}/api/dataset/{ds_name}')
+    data = json.loads(response.text)
+    expected = {"study": 1,
+                "shortName": "SweGen2",
+                "fullName": "SweGen2",
+                "version": {"version": "20190409",
+                            "description": "desc",
+                            "terms": "terms",
+                            "availableFrom": "2001-01-05",
+                            "refDoi": "doi",
+                            "dataContactName": "place",
+                            "dataContactLink": "email",
+                            "numVariants": None,
+                            "coverageLevels": [1, 5, 10, 15, 20, 25, 30, 50, 100],
+                            "portalAvail": True,
+                            "fileAccess": "REGISTERED",
+                            "beaconAccess": "PUBLIC",
+                            "dataset": 2,
+                            "referenceSet": 1,
+                            "varCallRef":None},
+                "future": False}
+    for value in expected:
+        assert data[value] == expected[value]
+    assert len(data) == 14
+
+    ds_name = 'Unrel'
+    response = requests.get(f'{BASE_URL}/api/dataset/{ds_name}')
+    assert not response.text
+    assert response.status_code == 404
+
