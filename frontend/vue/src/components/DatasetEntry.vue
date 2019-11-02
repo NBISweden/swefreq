d4efc8de7da8193c282def60fc4e5076fb5f71c0
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-11-02 19:32:04 +0100

    Add simple listing of datasets for front page.

---
+++ frontend/vue/src/components/DatasetEntry.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="row dataset-row">
+  <div class="col-md-1"></div>
+  <div class="col-md-10">
+    <div class="dataset-panel">
+      <div class="dataset-heading">{{ dataset.fullName }}</div>
+      <div class="dataset-body">
+        <div v-if="dataset.hasImage">
+          <img :src="'/api/dataset/' + dataset.shortName + '/logo'" class="dataset-logo" :alt="dataset.shortName + 'logo'" />
+        </div>
+        <div v-if="dataset.future" class='future-blurb'>This version will become public at {{ dataset.version.availableFrom }}.</div>
+        <div v-html="dataset.version.description" />
+      </div>
+      <div class="dataset-links">
+        <li><a :href="dataset.urlbase">More information</a></li>
+        <li><a :href="dataset.urlbase + '/beacon'">Beacon</a></li>
+        <li><a :href="dataset.urlbase + '/browser'">Graphical Browser</a></li>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-1"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'DatasetEntry',
+  data() {
+    return {
+    }
+  },
+  props: ["dataset"],
+};
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/vue/src/components/HomeComponent.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container">
+  <dataset-entry v-for="dataset in datasets" :key="dataset.id" :dataset="dataset"></dataset-entry>
+</div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+import DatasetEntry from '../components/DatasetEntry.vue';
+
+export default {
+  name: 'HomeComponent',
+  data() {
+    return {
+    }
+  },
+  computed: {
+    ...mapGetters(['datasets'])
+  },
+  components: {
+    'dataset-entry': DatasetEntry,
+  },
+  created() {
+    this.$store.dispatch('getDatasetList');
+  },
+};
+
+</script>
+
+<style scoped>
+.navigation-bar {
+    padding: 5px 0px}
+.navigation-link {
+    padding: 0px 10px;
+}
+</style>
