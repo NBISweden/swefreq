8d7ac184a29fdccc9e0581e2791b56d8c89fa6e4
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-11-15 09:18:52 +0100

    Keep browser tab selected when leaving the search page.

--- frontend/vue/src/components/dataset/DatasetViewerBar.vue
+++ frontend/vue/src/components/dataset/DatasetViewerBar.vue
@@ -5,7 +5,7 @@
     <li :class="{'active': this.$route.path.endsWith('/terms')}"><router-link :to="'/dataset/' + datasetName + '/terms'">Terms</router-link></li>
     <li :class="{'active': this.$route.path.endsWith('/download')}"><router-link :to="'/dataset/' + datasetName + '/download'">Dataset Access</router-link></li>
     <li :class="{'active': this.$route.path.endsWith('/beacon')}"><router-link :to="'/dataset/' + datasetName + '/beacon'">Beacon</router-link></li>
-    <li :class="{'active': this.$route.path.endsWith('/browser')}"><router-link :to="'/dataset/' + datasetName + '/browser'">Browser</router-link></li>
+    <li :class="{'active': this.$route.path.includes('/browser')}"><router-link :to="'/dataset/' + datasetName + '/browser'">Browser</router-link></li>
     <li :class="{'active': this.$route.path.endsWith('/admin'), 'pull-right': true, 'admin-tab': true}"><router-link :to="'/dataset/' + datasetName + '/admin'">Admin</router-link></li>
   </ul>
 </div>
