10011c695c24e53385a8d255a83690423dbe8bcf
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-03-07 14:28:23 +0100

    No need to have the before install file anymore

--- test/travis_before_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -x
-
-PSQL_VERSION="10"
-PSQL_PORT="5433"
-
-docker pull "postgres:$PSQL_VERSION"
-
-docker run --rm -e "POSTGRES_DB=swefreq" -d -p "$PSQL_PORT:5432" "postgres:$PSQL_VERSION"
