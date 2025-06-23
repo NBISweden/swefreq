e8eab193eb44cf4217eefb91526ccfb3e5689ccb
Johan Viklund <johan.viklund@nbis.se>
2025-06-23 13:08:31 +0200

    Update AAI to use LifeScience AAI instead of Elixir AAI
    This is because of Elixir AAI has been migrated to LifeScience AAI, a
    corresponding update to the database has been made in our production
    system already:
    
    UPDATE users.users SET identity=substring(identity, 1, 40) || '@lifescience-ri.eu' WHERE identity_type='elixir;

--- backend/auth.py
+++ backend/auth.py
@@ -32,9 +32,9 @@ class DeveloperLogoutHandler(BaseHandler):
 
 
 class ElixirLoginHandler(BaseHandler, tornado.auth.OAuth2Mixin):
-    _OAUTH_AUTHORIZE_URL = "https://login.elixir-czech.org/oidc/authorize"
-    _OAUTH_ACCESS_TOKEN_URL = "https://login.elixir-czech.org/oidc/token"
-    _OAUTH_USERINFO_ENDPOINT = "https://login.elixir-czech.org/oidc/userinfo"
+    _OAUTH_AUTHORIZE_URL = "https://login.aai.lifescience-ri.eu/oidc/authorize"
+    _OAUTH_ACCESS_TOKEN_URL = "https://login.aai.lifescience-ri.eu/oidc/token"
+    _OAUTH_USERINFO_ENDPOINT = "https://login.aai.lifescience-ri.eu/oidc/userinfo"
     _OAUTH_SETTINGS_KEY = 'elixir_oauth'
 
     def _generate_state(self):
